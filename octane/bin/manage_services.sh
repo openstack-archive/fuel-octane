42fdea1462207561cc82245b588ae8b81339894f
Oleg Gelbukh <ogelbukh@mirantis.com>
2015-02-05 14:17:05 +0000

    Manage OpenStack services on upgraded controllers
    Stop services before database upgrade.
    Set upgrade_levels with nova.conf.
    Start services after controllers switch over from original to upgraded.

---
+++ octane/bin/manage_services.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+set -x
+
+action=${1:-help}
+version=${2:-icehouse}
+hosts_file=${3:-controllers}
+pssh_run="pssh -i -h $hosts_file"
+
+stop_corosync_services() {
+    $pssh_run "crm status | awk '/clone/ {print \$4}' \
+        | tr -d [] | grep -vE '(mysql|haproxy)' \
+        | xargs -tI{} sh -c 'crm resource stop {}'"
+}
+
+stop_upstart_services() {
+    local command
+    command=$(cat <<EOF
+services="nova keystone heat neutron cinder glance"; \
+echo -n \$services \
+        | xargs -d" " -I{} sh -c 'ls /etc/init/{}* \
+        | grep -Ev override \
+        | sed -E "s,.*/([^\.]+)(\.conf|override)?$,\1," \
+        | sort -u | xargs -I@ sh -c "status @ \
+        | grep -v stop >/dev/null 2>&1 && echo @"' > services;
+    for s in \$(<services);
+        do
+            stop \$s;
+        done
+EOF
+)
+    $pssh_run "$command"
+}
+
+start_corosync_services() {
+    $pssh_run "pcs resource \
+    | awk '/Clone Set:/ {print \$4; getline; print \$1}' \
+    | sed 'N;s/\n/ /' | tr -d :[] \
+    | grep Stopped | awk '{print \$1}' \
+    | xargs -I@ sh -c \"crm resource start @\""
+}
+
+start_upstart_services() {
+    local command
+    command=$(cat <<EOF
+crm_services=\$(pcs resource \
+    | awk '/Clone Set:/ {print \$4; getline; print \$1}' \
+    | sed 'N;s/\n/ /' \
+    | tr -d ':[]' | awk '{print substr(\$1,3)}');
+for s in \$(<services);
+do
+    for cs in \$crm_services; do
+        if [ \$cs -eq \$s ]; then
+            break 2;
+        fi;
+    done;
+    start \$s;
+done;
+EOF
+)
+    $pssh_run "$command"
+}
+
+set_upgrade_levels() {
+    local version
+    version=${1:-icehouse}
+    upgrade_levels=$(cat <<EOF
+[upgrade_levels]
+compute=icehouse
+conductor=icehouse
+scheduler=icehouse
+EOF
+)
+    $pssh_run "echo $upgrade_levels >> /etc/nova/nova.conf"
+}
+
+display_help_message() {
+    echo "Usage: $0 COMMAND
+COMMANDS:
+    stop    - stop all openstack services on controller nodes
+    start   - start all openstack services on controller nodes
+    config VERSION  - update nova.conf with upgrade_levels configuration, VERSION
+                      defines original version of OpenStack (defaults to
+                      'icehouse')
+    help    - show this message"
+}
+
+case $action in 
+    stop)
+        stop_corosync_services
+        stop_upstart_services
+        ;;
+    config)
+        set_upgrade_levels $version
+        ;;
+    start)
+        start_corosync_services
+        start_upstart_services
+        ;;
+    help)
+        display_help_message
+        ;;
+    *)
+        display_help_message
+        ;;
+esac
