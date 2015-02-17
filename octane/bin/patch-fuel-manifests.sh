8912236a94525568ac97c34949e56f44808960b2
Oleg Gelbukh <ogelbukh@mirantis.com>
2015-02-17 13:47:30 +0000

    Patch astute post-deployment actions
    Disable uploading image to Glance in Astute as part of deployment process. This
    will allow to avoid including ceph-osd node in 6.0 deployment.

--- octane/bin/patch-fuel-manifests.sh
+++ octane/bin/patch-fuel-manifests.sh
@@ -3,6 +3,8 @@
 set -x
 
 modulespath="/etc/puppet/2014.2-6.0/modules"
+astutepath="/usr/lib64/ruby/gems/2.1.0/gems/astute-6.0.0/lib/astute/"
+upload_cirros_image_path="$astutepath/post_deployment_actions/upload_cirros_image.rb"
 
 sed -ie "s%skip_existing = false%skip_existing = true%" \
     $modulespath/l23network/manifests/l2/bridge.pp
@@ -10,3 +12,7 @@ sed -ie "s%defaultto(false)%defaultto(true)%" \
     $modulespath/l23network/lib/puppet/type/l2_ovs_bridge.rb
 sed -ie "s%(\$run_ping_checker) = .*$%\1 = false%" \
     $modulepath/osnailyfacter/mainfests/cluster_ha.pp
+
+dockerctl shell astute sed -i "90s%^%#%" $upload_cirros_image_path
+dockerctl shell astute sed -i "90i\ \ \ \ \ \ \ \ response['data']['exit_code'] = 0" \
+    $upload_cirros_image_path
