2397122fadf42573467e9d6bbcb4b9c2108620b6
Oleg Gelbukh <ogelbukh@mirantis.com>
2015-02-20 11:24:12 +0000

    Patch astute to disable image upload post deployment action

--- octane/bin/patch-fuel-manifests.sh
+++ octane/bin/patch-fuel-manifests.sh
@@ -4,7 +4,7 @@ set -x
 
 modulespath="/etc/puppet/2014.2-6.0/modules"
 astutepath="/usr/lib64/ruby/gems/2.1.0/gems/astute-6.0.0/lib/astute/"
-upload_cirros_image_path="$astutepath/post_deployment_actions/upload_cirros_image.rb"
+deploy_actions_path="$astutepath/deploy_actions.rb"
 
 sed -ie "s%skip_existing = false%skip_existing = true%" \
     $modulespath/l23network/manifests/l2/bridge.pp
@@ -13,6 +13,4 @@ sed -ie "s%defaultto(false)%defaultto(true)%" \
 sed -ie "s%(\$run_ping_checker) = .*$%\1 = false%" \
     $modulepath/osnailyfacter/mainfests/cluster_ha.pp
 
-dockerctl shell astute sed -i "90s%^%#%" $upload_cirros_image_path
-dockerctl shell astute sed -i "90i\ \ \ \ \ \ \ \ response['data']['exit_code'] = 0" \
-    $upload_cirros_image_path
+dockerctl shell astute sed -i "94s%^%#%" $deploy_actions_path
