c2df5ba5668b7699ec7808ae3f972da3a5d5be43
Ryabin Sergey <sryabin@mirantis.com>
2015-02-26 18:42:38 +0300

    Host evacuation script

---
+++ octane/bin/host_evacuation.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -ex
+
+
+if [ -z "$1" ]; then
+        echo "Usage $0 <evacuation_host>"
+        exit 2
+fi
+
+nova service-list --host $1
+
+nova service-list | grep -q 'nova-compute.*enabled' && {
+        nova service-disable $1 nova-compute
+}
+
+nova service-list | grep -q 'nova-compute.*enabled' || {
+        echo "All nova-compute are disabled"
+        exit 3
+}
+
+nova list --host $1 | grep ' ACTIVE ' | cut -d\| -f3 | xargs -I% nova live-migration %
