ff2b582f41386823dcc7601222531a48f499cd55
Ilya Kharin <akscram@gmail.com>
2015-03-05 11:39:46 +0300

    Fresh ceph manifest and proper location for them

--- puppet/update.sh
+++ puppet/update.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -ex
-cp ./ceph_osd.rb /etc/puppet/modules/ceph/lib/facter/ceph_osd.rb
-cp ./init.pp /etc/puppet/modules/ceph/manifests/init.pp
-cp ./osd.pp /etc/puppet/modules/ceph/manifests/osd.pp
+cp ./ceph_osd.rb /etc/puppet/2014.2-6.0/modules/ceph/lib/facter/ceph_osd.rb
+cp ./init.pp /etc/puppet/2014.2-6.0/modules/ceph/manifests/init.pp
+cp ./osd.pp /etc/puppet/2014.2-6.0/modules/ceph/manifests/osd.pp
