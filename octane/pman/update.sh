a6630c7aae99fed21615bdfbac2eceaaf4f4c128
Ilya Kharin <akscram@gmail.com>
2015-03-13 13:12:10 +0300

    Update nailgun
    Now we do not update cobbler's snippets because pmanager.py will be
    rewrited accordint previous changes in nailgun.volume.manager.

--- octane/pman/update.sh
+++ octane/pman/update.sh
@@ -1,20 +1,33 @@
 #!/bin/sh -ex
 
-tmp=/tmp/update.sh.tmp
+run=.state
 
-function copy() {
+function copyfile() {
     local src=$1 dst=$2
-    while true
-    do
-        dockerctl copy $dst $tmp
-        cmp -s $src $tmp && break
-        dockerctl copy $src $dst
+    local tmp="$run/`basename $dst`"
+    for i in `seq 1 3`; do
+        while true
+        do
+            dockerctl copy $dst $tmp
+            cmp -s $src $tmp && break
+            cp $tmp "$tmp.backup"
+            dockerctl copy $src $dst
+            sleep 1
+        done
         sleep 1
     done
 }
 
+function patchfile() {
+    local orig=$1 patchf=$2
+    local tmp="$run/`basename $orig`"
+    dockerctl copy $orig $tmp
+    patch -Np4 $tmp $patchf && cp $tmp "$tmp.backup" || return 0
+    dockerctl copy $tmp $orig
+}
+
+mkdir -p $run
 dockerctl restart cobbler
-sleep 10
-copy ./pmanager.py cobbler:/usr/lib/python2.6/site-packages/cobbler/pmanager.py
-copy ./ubuntu_partition cobbler:/var/lib/cobbler/snippets/ubuntu_partition
-copy ./ubuntu_partition_late cobbler:/var/lib/cobbler/snippets/ubuntu_partition_late
+copyfile ./pmanager.py cobbler:/usr/lib/python2.6/site-packages/cobbler/pmanager.py
+patchfile nailgun:/usr/lib/python2.6/site-packages/nailgun/volumes/manager.py manager.py.patch
+dockerctl shell nailgun pkill -f wsgi
