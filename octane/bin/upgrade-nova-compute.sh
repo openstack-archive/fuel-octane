524c0910abfbbd928f4c68cb3bace05443823d15
Ryabin Sergey <sryabin@mirantis.com>
2015-03-20 18:35:42 +0300

    Nova.conf patch

--- octane/bin/upgrade-nova-compute.sh
+++ octane/bin/upgrade-nova-compute.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 extract_vars() {
-        sed -re '/^\+.*%.*/ s/.*%([^%]+)%.*/\L\1/;tx;d;:x' $PATCH
+        sed -re '/^\+.*%.*/ s/.*%([^%]+)%.*/\L\1/;tx;d;:x' $1
 }
 
 convert_vars_to_regex() {
@@ -8,16 +8,19 @@ convert_vars_to_regex() {
 }
 
 generate_template_regex() {
-        ssh root@$1 find /etc/neutron -type f -exec cat {} \;| egrep "`extract_vars | convert_vars_to_regex`" {}  | awk -F= '{key = gensub(" ", "", "g", $1); printf("s|%%%s%%|%s|g;", toupper(key), $2)}'
+        egrep "`extract_vars $1 | convert_vars_to_regex`" | awk -F= '{key = gensub(" ", "", "g", $1); printf("s|%%%s%%|%s|g;", toupper(key), $2)}'
 }
 
 upgrade_compute_service() {
 	local regex
-	regex=$(generate_template_regex $1)
+	local nova_regex
+	regex=$(ssh root@$1 "find /etc/neutron -type f -exec cat {} \;" | generate_template_regex $PATCH)
+	nova_regex=$(ssh root@$1 "cat /etc/nova/nova.conf" | generate_template_regex $NOVA_PATCH)
 	sed -r "$regex" ${PATCH}  | ssh root@$1 "tee /tmp/patch-neutron-config_$1.patch"
 	ssh root@$1 "apt-get update; apt-get install -o Dpkg::Options::='--force-confnew' --yes nova-compute"
 	ssh root@$1 "cd /etc/neutron && patch -p0 < /tmp/patch-neutron-config_$1.patch"
-}
+	cat ${NOVA_PATCH} | sed -r "${nova_regex}" | ssh root@$1 "cat > /etc/nova/nova.conf"
+} 
 
 add_apt_sources() {
 	local source
@@ -30,9 +33,10 @@ add_apt_sources() {
 
 [ -z "$1" ] && die "No node ID provided, exiting"
 PATCH=${2-"../patches/neutron-upgrade.patch"}
+NOVA_PATCH=${3-"../patches/nova.conf"}
 
-if [ ! -f "$PATCH" ]; then
-    echo "Usage $0 NODE_ID [PATCH_PATH]" >> /dev/stderr
+if [ ! -f "$PATCH" -o ! -f "$NOVA_PATCH" ]; then
+    echo "Usage $0 NODE_ID [NEUTRON_PATCH_PATH] [NOVA_PATCH_PATH]" >> /dev/stderr
     exit 1
 fi
 
