d99570e45459450fe7542bb81980d1aa6948adb0
Oleg Gelbukh <ogelbukh@mirantis.com>
2015-05-25 09:49:33 +0000

    Typo in astute patch script

--- octane/patches/astute/update.sh
+++ octane/patches/astute/update.sh
@@ -7,7 +7,7 @@ PATCH_DIR=$(dirname $0)
 CONTAINER="astute"
 SRC_PATH="/usr/lib64/ruby/gems/2.1.0/gems/astute-6.0.0/lib/astute/"
 
-dockerctl restart $(CONTAINER)
+dockerctl restart ${CONTAINER}
 sleep 10
 patchfile ${CONTAINER}:${SRC_PATH}/deploy_actions.rb \
     ${PATCH_DIR}/deploy_actions.rb.patch
