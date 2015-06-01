2e8a73846dd9b5cde9a9d8927b8f2f7995d94503
Oleg Gelbukh <ogelbukh@mirantis.com>
2015-06-01 11:44:12 +0000

    Always SSH to IP address
    Connecting to hostname can cause issues if for some reason resolving of those
    names is broken. Make ssh always connect to IP address defined in Fuel
    API/client to that server, instead of hostname.

--- octane/lib/db.sh
+++ octane/lib/db.sh
@@ -32,15 +32,19 @@ xtrabackup_from_env() {
 
 xtrabackup_restore_to_env() {
     [ -z "$1" ] && die "No env ID provided, exiting"
-    local cics=$(list_nodes $1 controller)
-    echo $cics | xargs -I{} ssh root@{} \
+    local cic
+    local cics="$(list_nodes $1 controller)"
+    for cic in $(echo "$cics");
+    do
+         ssh root@$(get_host_ip_by_node_id ${cic#node-}) \
         "mv /var/lib/mysql/grastate.dat /var/lib/mysql/grastate.old"
-    local primary_cic=$(echo $cics | head -1)
-    cat /tmp/dbs.original.tar.gz \
-        | ssh root@$primary_cic "cat - > /var/lib/mysql/dbs.original.tar.gz"
-    ssh root@$primary_cic \
+    done
+    local primary_cic=$(echo "$cics" | head -1)
+    scp /tmp/dbs.original.tar.gz \
+        root@$(get_host_ip_by_node_id ${primary_cic#node-}):/var/lib/mysql
+    ssh root@$(get_host_ip_by_node_id ${primary_cic#node-}) \
         "cd /var/lib/mysql;
-        tar -zxvf db.original.tar.gz;
+        tar -zxvf dbs.original.tar.gz;
         chown -R mysql:mysql /var/lib/mysql;
         export OCF_RESOURCE_INSTANCE=p_mysql;
         export OCF_ROOT=/usr/lib/ocf;
@@ -48,16 +52,19 @@ xtrabackup_restore_to_env() {
         export OCF_RESKEY_additional_parameters="\""--wsrep-new-cluster"\"";
         /usr/lib/ocf/resource.d/mirantis/mysql-wss start;"
     db_sync ${primary_cic#node-}
-    echo $cics | grep -v $primary_cic | xargs -I{} ssh root@{} \
+    for cic in $(echo "$cics" | grep -v $primary_cic);
+    do
+        ssh root@$(get_host_ip_by_node_id ${cic#node-}) \
         "export OCF_RESOURCE_INSTANCE=p_mysql;
         export OCF_ROOT=/usr/lib/ocf;
         export OCF_RESKEY_socket=/var/run/mysqld/mysqld.sock;
         /usr/lib/ocf/resource.d/mirantis/mysql-wss start;"
+    done
 }
 
 db_sync() {
     [ -z "$1" ] && die "No node ID provided, exiting"
-    ssh root@node-$1 "keystone-manage db_sync;
+    ssh root@$(get_host_ip_by_node_id $1) "keystone-manage db_sync;
 nova-manage db sync;
 heat-manage db_sync;
 neutron-db-manage --config-file=/etc/neutron/neutron.conf upgrade head;
