70c4d43331f0205a0c12f00e994bd9181b3d7975
Ilya Kharin <akscram@gmail.com>
2015-06-05 16:07:16 +0300

    Add patch for urls.py

--- octane_nailgun/tools/push.sh
+++ octane_nailgun/tools/push.sh
@@ -13,6 +13,11 @@ git push --force $host HEAD
 
 ssh $host "cd ${location}; git reset --hard $branch"
 ssh $host "cd ${location}; git clean -x -d -f"
+
+id=$(ssh $host "docker inspect -f='{{if .ID}}{{.ID}}{{else}}{{.Id}}{{end}}' ${container}")
+rootfs="/var/lib/docker/devicemapper/mnt/${id}/rootfs"
+ssh $host "patch -bV numbered -Np1 -d $rootfs < ${location}/tools/urls.py.patch || :"
+
 ssh $host "cd ${location}; python setup.py bdist_wheel"
 
 ssh $host "cd ${location}; dockerctl copy dist/${wheel} nailgun:/root/${wheel}"
