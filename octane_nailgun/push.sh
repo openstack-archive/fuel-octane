03460ad43ce1fecb83d0c65b03c9cd1d093f0042
Ilya Kharin <akscram@gmail.com>
2015-06-05 14:07:10 +0300

    Do not use dockerctl

--- octane_nailgun/push.sh
+++ octane_nailgun/push.sh
@@ -3,6 +3,7 @@
 host=${1:-"cz5545-fuel"}
 branch=${2:-$(git rev-parse --abbrev-ref HEAD)}
 
+container="fuel-core-6.1-nailgun"
 version=$(awk -F\" '/version/{print $2}' < setup.py)
 wheel="octane_nailgun-${version}-py2-none-any.whl"
 
@@ -12,9 +13,7 @@ git push --force $host HEAD
 ssh $host "cd octane; git reset --hard $branch"
 ssh $host "cd octane; git clean -x -d -f"
 ssh $host "cd octane/octane_nailgun; python setup.py bdist_wheel"
-# XXX: Here we can use directly `cp` and `pip` because the rootfs of
-#       the container is located in
-#       /var/lib/docker/devicemapper/mnt/<ID>/rootfs/.
-ssh $host "dockerctl copy dist/${wheel} nailgun:/root/"
-ssh $host "dockerctl shell nailgun pip install -U ${wheel}"
-ssh $host "docker restart fuel-core-6.1-nailgun"
+
+ssh $host "docker cp ${container}:/root/ dist/${wheel}"
+ssh $host "docker exec ${container} pip install -U ${wheel}"
+ssh $host "docker restart ${container}"
