c4530ed8d484565ec1db05a1ca7457b2d446662c
Ilya Kharin <akscram@gmail.com>
2015-06-09 10:44:02 +0300

    Properly copy common parameters

--- octane_nailgun/octane/handlers.py
+++ octane_nailgun/octane/handlers.py
@@ -14,12 +14,17 @@ class ClusterCloneHandler(base.BaseHandler):
         params = self.checked_data()
         cluster = self.get_object_or_404(self.single, cluster_id)
         release = self.get_object_or_404(objects.Release, params["release_id"])
-        clone = self.single.create({
+        data = {
             "name": params["name"],
-            "mode": consts.CLUSTER_MODES.ha_full,
+            "mode": cluster.mode,
             "status": consts.CLUSTER_STATUSES.new,
-            "net_provider": consts.CLUSTER_NET_PROVIDERS.neutron,
+            "net_provider": cluster.net_provider,
             "grouping": consts.CLUSTER_GROUPING.roles,
             "release_id": release.id,
-        })
+        }
+        if cluster.net_provider == consts.CLUSTER_NET_PROVIDERS.neutron:
+            data["net_segmentation_type"] = \
+                cluster.network_config.segmentation_type
+            data["net_l23_provider"] = cluster.network_config.net_l23_provider
+        clone = self.single.create(data)
         return self.single.to_json(clone)
