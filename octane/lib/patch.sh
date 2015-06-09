720de5e1a8ac1ce46435cad7fcecfe32c7525d8a
Oleg Gelbukh <ogelbukh@mirantis.com>
2015-06-09 19:05:56 +0000

    Function to patch specified components of Fuel
    This function accepts names of directories that contain patches to components of
    Fuel installer. Each directory under `octane/patches' must contain script named
    `update.sh' that applies patches, and patch files to be applied.

--- octane/lib/patch.sh
+++ octane/lib/patch.sh
@@ -42,3 +42,15 @@ patchfile() {
     [ -z "$2" ] && die "No patch file provided, exiting"
     patch -Np1 --dry-run --silent $1 $2 2>/dev/null && patch -Np1 $1 $2
 }
+
+patch_fuel_components() {
+    local cmp
+    [ -z "$1" ] && die "No component name provided, exiting"
+    for cmp in "$@";
+    do
+        [ -d "$PATCH_DIR/$cmp" ] || die "No dir for component $cmp, exiting"
+        pushd "$PATCH_DIR/$cmp"
+        [ -x "./update.sh" ] && ./update.sh
+        popd
+    done
+}
