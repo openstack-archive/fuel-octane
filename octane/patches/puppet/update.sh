bbc6f7bc15246f44a743ccc96adb3b32dd593115
Oleg Gelbukh <ogelbukh@mirantis.com>
2015-06-09 15:03:00 +0000

    Remove global vars and repair path to Puppet patches

--- octane/patches/puppet/update.sh
+++ octane/patches/puppet/update.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -ex
-PATCH_DIR=$(dirname `readlink -f $0`)
-. $PATCH_DIR/../../lib/patch
-PUPPET_PATH="/etc/puppet/2014.2.2-6.1/modules"
-
 pushd $PUPPET_PATH
-find $PATCH_DIR -maxdepth 1 -mindepth 1 -type d \
+find $PATCH_DIR/puppet/ -maxdepth 1 -mindepth 1 -type d \
     | xargs -I{} bash -c "patch -Np3 < {}/patch"
 popd
