6cf942a42638487aee13533fd36441036a46a30d
Ilya Kharin <akscram@gmail.com>
2015-06-10 20:43:58 +0300

    At first try to install octane pkg

--- octane_nailgun/tools/push.sh
+++ octane_nailgun/tools/push.sh
@@ -14,12 +14,12 @@ git push --force "$host" "$branch"
 ssh $host "cd ${location}; git reset --hard $branch"
 ssh $host "cd ${location}; git clean -x -d -f"
 
+ssh $host "cd ${location}; python setup.py bdist_wheel"
+ssh $host "cd ${location}; dockerctl copy dist/${wheel} nailgun:/root/${wheel}"
+ssh $host "docker exec ${container} pip install -U ${wheel}"
+
 id=$(ssh $host "docker inspect -f='{{if .ID}}{{.ID}}{{else}}{{.Id}}{{end}}' ${container}")
 rootfs="/var/lib/docker/devicemapper/mnt/${id}/rootfs"
 ssh $host "patch -bV numbered -Np1 -d $rootfs < ${location}/tools/urls.py.patch || :"
 
-ssh $host "cd ${location}; python setup.py bdist_wheel"
-
-ssh $host "cd ${location}; dockerctl copy dist/${wheel} nailgun:/root/${wheel}"
-ssh $host "docker exec ${container} pip install -U ${wheel}"
 ssh $host "dockerctl shell ${container} pkill -f wsgi"
