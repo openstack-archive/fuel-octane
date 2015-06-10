5cf2fc16f665402e5093dc6e129b0d2fc7d95e3e
Ilya Kharin <akscram@gmail.com>
2015-06-10 20:58:03 +0300

    Fix bootstrap.sh commands

--- octane_nailgun/tools/bootstrap.sh
+++ octane_nailgun/tools/bootstrap.sh
@@ -4,8 +4,8 @@ host=${1:-"cz5545-fuel"}
 location=${2:-"/root/octane"}
 branch=${3:-$(git rev-parse --abbrev-ref HEAD)}
 
-ssh $host "yum install -y git" \
-          "pip install wheel" \
+ssh $host "yum install -y git;" \
+          "pip install wheel;" \
           "mkdir -p ${location};" \
           "git init ${location};" \
           "git config --file ${location}/.git/config receive.denyCurrentBranch warn;"
