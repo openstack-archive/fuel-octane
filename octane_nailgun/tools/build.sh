470f3a860a7ac5f83fb837f976d262e58179b35a
Ilya Kharin <akscram@gmail.com>
2015-06-17 14:49:28 +0300

    Patch nailgun/../urls.py during build of image

--- octane_nailgun/tools/build.sh
+++ octane_nailgun/tools/build.sh
@@ -3,15 +3,15 @@
 host=${1:-"cz5545-fuel"}
 branch=${2:-$(git rev-parse --abbrev-ref HEAD)}
 
-version=$(awk -F\" '/version/{print $2}' < setup.py)
-wheel="octane_nailgun-${version}-py2-none-any.whl"
-
-location="octane/octane_nailgun"
-container="fuel-core-6.1-nailgun"
+remote="$(git remote -v | awk "/$host/ && /fetch/{print \$2}")"
+location="${remote#ssh://$host}/octane_nailgun"
 
 git push --force $host HEAD
 
-ssh $host "cd ${location}; git reset --hard $branch"
-ssh $host "cd ${location}; git clean -x -d -f"
-ssh $host "cd ${location}; python setup.py bdist_wheel"
-ssh $host "cd ${location}; docker build -t octane/nailgun_6.1 ."
+ssh $host \
+    "set -ex;" \
+    "cd ${location};" \
+    "git reset --hard $branch;" \
+    "git clean -x -d -f;" \
+    "python setup.py bdist_wheel;" \
+    "docker build -t octane/nailgun_6.1 .;" \
