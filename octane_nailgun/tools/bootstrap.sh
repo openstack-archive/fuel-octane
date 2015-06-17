b0a02c7a8b75eb542deb8f388dab10f37a9bd8a1
Ilya Kharin <akscram@gmail.com>
2015-06-17 14:47:25 +0300

    Add patch pkg during bootstrap

--- octane_nailgun/tools/bootstrap.sh
+++ octane_nailgun/tools/bootstrap.sh
@@ -6,7 +6,7 @@ branch=${3:-$(git rev-parse --abbrev-ref HEAD)}
 
 ssh $host \
     "set -ex;" \
-    "yum install -y git python-pip;" \
+    "yum install -y git python-pip patch;" \
     "pip install wheel;" \
     "mkdir -p ${location};" \
     "git init ${location};" \
