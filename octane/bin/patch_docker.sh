b33ee726ee8e41a7b3215707326d2e5e43ecf513
Ryabin Sergey <sryabin@mirantis.com>
2015-06-19 16:57:59 +0300

    Add patch to dockerfile

--- octane/bin/patch_docker.sh
+++ octane/bin/patch_docker.sh
@@ -1,69 +1,13 @@
-confdir="/etc/dockerctl"
-. ${confdir}/config
-. /usr/share/dockerctl/functions
+docker_patch() {
+    local CONTAINER=$1
 
-#alias extract_patch_files="sed -nr '/\+{3}/ {s,\+{3} (b/)?([^\t]+).*,\2,g;p}'"
+    test -d ../docker/${CONTAINER} || {
+        echo "${CONTAINER} invalid argument"
+        return 1
+    } 
 
-function extract_files_from_docker() {
-	local container_name=$1
-	shift
-	shell_container ${container_name} tar cvf - $*
+    cp /usr/bin ../docker/${CONTAINER}/resources/ 
+    tag=`grep -Eim1 ^from ../docker/${CONTAINER}/Dockerfile | awk '{print $2 "_upgrade"}'`
+    docker build ../docker/${CONTAINER} 
+    
 } 
-
-function put_files_to_docker() {
-	local container_name=$1
-	local prefix=$2
-	local source=$3
-	(cd ${source} && tar cvf - * ) | shell_container ${container_name} tar -xv --overwrite -f - -C ${prefix} 
-} 
-
-function extract_patch_files() {
-	sed -nr '/\+{3}/ {s,\+{3} (b/)?([^\t]+).*,\2,g;p}'
-} 
-
-
-# example docker_patch cobbler / patches/hostname.patch patches/another.patch
-function docker_patch() {
-	local container=$1
-	local prefix=$2
-	shift 2
-	local patchs=$*
-
-	list_containers | egrep -q "^${container}$" || {
-		echo "Container $container not found" > /dev/stderr
-		return
-	} 
-
-	for p in $patchs; do
-		test -f $p || {
-			echo "Patch $p not found" > /dev/stderr
-			return
-		} 
-	done
-
-
-	patch_dir=$(mktemp -d /tmp/docker_patch.XXXXXXX)
-
-	patch_files=`cat $patchs | extract_patch_files | sed -r "/^[^\/]/ { s ^ ${prefix}/ g }"`
-
-
-	extract_files_from_docker ${container} ${patch_files} | tar xvf - -C ${patch_dir}
-
-	cat ${patchs} | patch -p0 -d ${patch_dir} && put_files_to_docker ${container} ${prefix} ${patch_dir} 
-
-	test -d $patch_dir && rm -rf ${patch_dir} 
-} 
-#set -x
-
-CONTAINER_NAME=$1
-PATCH_PREFIX=$2
-shift 2
-PATCHS=$*
-
-test -z "$CONTAINER_NAME" -o -z "${PATCH_PREFIX}" -o -z "${PATCHS} " && {
-	echo "Usage $0 <container_name> <patch_prefix> <patch1> ...." > /dev/stderr
-	exit 2
-} 
-
-docker_patch $CONTAINER_NAME $PATCH_PREFIX $*
-
