88b780f5101936e11ae1d1ae379958b94483d87d
Ryabin Sergey <sryabin@mirantis.com>
2015-06-30 15:54:02 +0300

    Patch urls.py

--- octane/lib/docker.sh
+++ octane/lib/docker.sh
@@ -21,7 +21,6 @@ function put_files_to_docker() {
 	(cd ${source} && tar cvf - * ) | shell_container ${container_name} tar -xv --overwrite -f - -C ${prefix} 
 } 
 
-
 # example docker_patch cobbler / patches/hostname.patch patches/another.patch
 function do_docker_patch() {
 	local container=$1
@@ -44,12 +43,14 @@ function do_docker_patch() {
 
 	patch_dir=$(mktemp -d /tmp/docker_patch.XXXXXXX)
 
+
 	patch_files=`cat $patchs | extract_patch_files | sed -r "/^[^\/]/ { s ^ ${prefix}/ g }"`
+	prefix=`dirname $patch_files`
 
 
 	extract_files_from_docker ${container} ${patch_files} | tar xvf - -C ${patch_dir}
 
-	cat ${patchs} | patch -p0 -d ${patch_dir}/${prefix} && put_files_to_docker ${container} ${prefix} ${patch_dir} 
+	sed -r '/\+{3}/ {s,(.*/)([^ ]+) .*,+++ \2,g}' ${patchs} | patch -N -p0 -d ${patch_dir}/${prefix} && put_files_to_docker ${container} ${prefix} ${patch_dir} 
 
 	test -d $patch_dir && rm -rf ${patch_dir} 
 } 
@@ -72,6 +73,7 @@ function patch_all_containers() {
        docker_patch astute /usr/lib64/ruby/gems/2.1.0/gems/astute-6.1.0/lib/astute ${CWD}/docker/astute/resources/deploy_actions.rb.patch
        docker_patch cobbler /usr/lib/python2.6/site-packages/cobbler ${CWD}/docker/cobbler/resources/pmanager.py.patch
        docker_patch nailgun /usr/lib/python2.6/site-packages/nailgun/volumes ${CWD}/docker/nailgun/resources/manager.py.patch
+       docker_patch nailgun / ${CWD}/../octane_nailgun/tools/urls.py.patch
 } 
 
 
