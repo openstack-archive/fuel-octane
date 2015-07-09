57b55633460961b0853d0105909bb88039d04eea
Ryabin Sergey <sryabin@mirantis.com>
2015-07-09 15:48:46 +0300

    Fix forgotten dependenices

--- octane/lib/functions.sh
+++ octane/lib/functions.sh
@@ -2,7 +2,8 @@
 
 prepare_fuel_master() {
     [ -d "${FUEL_CACHE}" ] || mkdir -p ${FUEL_CACHE}
-    yum -y install postgresql.x86_64 pssh patch
+    yum -y install postgresql.x86_64 pssh patch python-pip 
+    pip install wheel
     install_octane_fuelclient
     patch_fuel_components puppet
     patch_all_containers
