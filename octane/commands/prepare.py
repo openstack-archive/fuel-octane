523e395fc21ce8c28e4982591701a5770bb74026
Yuriy Taraday <yorik.sar@gmail.com>
2015-07-16 11:13:22 +0300

    Finish rewriting prepare method
    Also finished with docker helper lib

--- octane/commands/prepare.py
+++ octane/commands/prepare.py
@@ -1,5 +1,72 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import glob
+import os.path
+import subprocess
+
 from cliff import command as cmd
+
+from octane.util import docker
+
+PACKAGES = ["postgresql.x86_64", "pssh", "patch", "python-pip"]
+PATCHES = [
+    ("astute", "/usr/lib64/ruby/gems/2.1.0/gems/astute-6.1.0/lib/astute",
+     "docker/astute/resources/deploy_actions.rb.patch"),
+    ("cobbler", "/usr/lib/python2.6/site-packages/cobbler",
+     "docker/cobbler/resources/pmanager.py.patch"),
+    ("nailgun", "/usr/lib/python2.6/site-packages/nailgun/volumes",
+     "docker/nailgun/resources/manager.py.patch"),
+    ("nailgun", "/", "../octane_nailgun/tools/urls.py.patch"),
+]
+# TODO: use pkg_resources for patches
+CWD = os.path.join(os.path.dirname(__file__), "..", "..")  # FIXME
+
+
+def patch_puppet():
+    puppet_dir = "/etc/puppet/2014.2.2-6.1/modules"
+    puppet_patch_dir = os.path.join(CWD, "patches", "puppet")
+    for d in os.listdir(puppet_patch_dir):
+        if not os.path.isdir(d):
+            continue
+        with open(os.path.join(puppet_patch_dir, d, "patch")) as patch:
+            subprocess.check_call(["patch", "-Np3"], stdin=patch,
+                                  cwd=puppet_dir)
+
+
+def install_octane_nailgun():
+    octane_nailgun = os.path.join(CWD, '..', 'octane_nailgun')
+    subprocess.check_call(["python", "setup.py", "bdist_wheel"],
+                          cwd=octane_nailgun)
+    wheel = glob.glob(os.path.join(octane_nailgun, 'dist', '*.whl'))[0]
+    subprocess.check_call(["dockerctl", "copy", wheel, "nailgun:/root/"])
+    docker.run_in_container("nailgun", ["pip", "install", "-U",
+                                        "/root/" + os.path.basename(wheel)])
+    docker.run_in_container("nailgun", ["pkill", "-f", "wsgi"])
+
+
+def prepare():
+    subprocess.check_call(["yum", "-y"] + PACKAGES)
+    subprocess.check_call(["pip", "install", "wheel"])
+    octane_fuelclient = os.path.join(CWD, '..', 'octane_fuelclient')
+    subprocess.check_call(["pip", "install", "-U", octane_fuelclient])
+    patch_puppet()
+    # From patch_all_containers
+    for container, prefix, patch in PATCHES:
+        docker.apply_patches(container, prefix, os.path.join(CWD, patch))
+    docker.run_in_container("astute", ["supervisorctl", "restart", "astute"])
+    install_octane_nailgun()
+
 
 class PrepareCommand(cmd.Command):
     def take_action(self, parsed_args):
-        pass
+        prepare()
--- octane/util/docker.py
+++ octane/util/docker.py
@@ -1,13 +1,32 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
 import contextlib
 import os.path
 import subprocess
 import tarfile
+import tempfile
 
 
 def in_container(container, args, **popen_kwargs):
     """Create Popen object to run command defined by list args in container"""
     return subprocess.Popen(["dockerctl", "shell", container] + args,
                             **popen_kwargs)
+
+
+def run_in_container(container, args, **popen_kwargs):
+    """Run command defined by list args in container and fail if it fails"""
+    subprocess.check_call(["dockerctl", "shell", container] + args,
+                          **popen_kwargs)
 
 
 def compare_files(container, local_filename, docker_filename):
@@ -24,9 +43,10 @@ def compare_files(container, local_filename, docker_filename):
 
 def find_files(source_dir):
     """Recursively find all files in source_dir
-    
+
     Returns tuple of full path and path relative to source_dir for each
-    file."""
+    file.
+    """
     for cur_dir, subdirs, files in os.walk(source_dir):
         assert cur_dir.startswith(source_dir)
         new_dir = cur_dir[len(source_dir) + 1:]
@@ -44,15 +64,80 @@ def put_files_to_docker(container, prefix, source_dir):
         stdin=subprocess.PIPE,
     )
     tar = tarfile.TarFile(fileobj=proc.stdin, mode='w')
-    with contextlib.closing(tar):
+    with contextlib.closing(tar):  # On 2.6 TarFile isn't context manager
         for local_filename, docker_filename in find_files(source_dir):
             tar.add(local_filename, docker_filename)
     proc.wait()
     assert proc.returncode == 0  # Don't inline with proc.wait()!
     for local_filename, docker_filename in find_files(source_dir):
-    for cur_dir, subdirs, files in os.walk(source_dir):
         if not compare_files(container, local_filename, docker_filename):
             raise Exception(
                 "Contents of {0} differ from contents of {1} in container {2}"
                 .format(local_filename, docker_filename, container)
             )
+
+
+def get_files_from_docker(container, files, destination_dir):
+    """Get files in 'files' list from container to destination_dir"""
+    # TODO: watch after stderr here
+    proc = in_container(
+        container,
+        ["tar", "-cvf", "-"] + files,
+        stdout=subprocess.PIPE,
+    )
+    tar = tarfile.TarFile(fileobj=proc.stdout, mode='r')
+    with contextlib.closing(tar):  # On 2.6 TarFile isn't context manager
+        tar.extractall(destination_dir)
+    proc.wait()
+    assert proc.returncode == 0
+
+
+def get_files_from_patch(patch):
+    """Get all files touched by a patch"""
+    result = []
+    with open(patch) as p:
+        for line in p:
+            if line.startswith('+++'):
+                fname = line[4:].strip()
+                if fname.startswith('b/'):
+                    fname = fname[2:]
+                result.append(fname)
+    return result
+
+
+def apply_patches(container, prefix, *patches):
+    """Apply set of patches to a container's filesystem"""
+    # TODO: review all logic here to apply all preprocessing steps to patches
+    # beforehand
+    tempdir = tempfile.mkdtemp(prefix='octane_docker_patches.')
+    try:
+        files = []
+        for patch in patches:
+            for fname in get_files_from_patch(patch):
+                if fname.startswith(prefix):
+                    files.append(fname[len(prefix) + 1:])
+                else:
+                    files.append(fname)
+        prefix = os.path.dirname(files[0])  # FIXME: WTF?!
+        get_files_from_docker(container, files, tempdir)
+        # TODO: watch after stdout/stderr here
+        proc = subprocess.Popen(
+            ["patch", "-N", "-p0", "-d", tempdir + "/" + prefix],
+            stdin=subprocess.PIPE,
+        )
+        for patch in patches:
+            with open(patch) as p:
+                for line in p:
+                    if line.startswith('+++'):  # FIXME: PLEASE!
+                        try:
+                            slash_pos = line.index('/', start=4)
+                            space_pos = line.index(' ', start=slash_pos)
+                        except ValueError:
+                            pass
+                        else:
+                            line = ('+++ ' + line[slash_pos + 1:space_pos] +
+                                    '\n')
+                    proc.stdin.write(line)
+        put_files_to_docker(container, "/", tempdir)
+    finally:
+        os.removedirs(tempdir)
