5a5e825fc8f9c53202869895bafb363f0ee3ed96
Yuriy Taraday <yorik.sar@gmail.com>
2015-07-23 19:53:02 +0300

    Abstract everything in subprocess, prepare to land SSH there

--- octane/util/subprocess.py
+++ octane/util/subprocess.py
@@ -31,21 +31,19 @@ def _close_stdin(chain_fn=None):
         chain_fn()
 
 
-class _LogPipe(threading.Thread):
+class _BaseLogPipe(threading.Thread):
     def __init__(self, level):
-        super(_LogPipe, self).__init__()
+        super(_BaseLogPipe, self).__init__()
         self.log_name = None
         self.log_level = level
-        self.read_fd, self.write_fd = os.pipe()
 
     def start(self, name):
-        os.close(self.write_fd)
         self.log_name = name
-        super(_LogPipe, self).start()
+        super(_BaseLogPipe, self).start()
 
     def run(self):
         try:
-            with io.open(self.read_fd, 'r', errors='replace') as pipe:
+            with self.pipe() as pipe:
                 for line in pipe:
                     if line.endswith('\n'):
                         line = line[:-1]
@@ -54,30 +52,99 @@ class _LogPipe(threading.Thread):
             LOG.exception("Exception in _LogPipe thread %s", self.name)
 
 
+class _LogPipe(_BaseLogPipe):
+    def __init__(self, level):
+        super(_LogPipe, self).__init__(level)
+        self.read_fd, self.write_fd = os.pipe()
+
+    def start(self, name):
+        os.close(self.write_fd)
+        super(_LogPipe, self).start(name)
+
+    def pipe(self):
+        return io.open(self.read_fd, 'r', errors='replace')
+
+
+class BasePopen(object):
+    def __init__(self, name, cmd, popen_kwargs):
+        self.name = name
+        self.cmd = cmd
+        self.popen_kwargs = popen_kwargs
+        self.stdin = None
+        self.stdout = None
+        self.stderr = None
+
+    def poll(self):
+        raise NotImplementedError("poll")
+
+    def wait(self):
+        raise NotImplementedError("wait")
+
+    def terminate(self):
+        raise NotImplementedError("terminate")
+
+    def communicate(self):
+        raise NotImplementedError("communicate")
+
+
+class LocalPopen(BasePopen):
+    def __init__(self, name, cmd, popen_kwargs):
+        super(LocalPopen, self).__init__(name, cmd, popen_kwargs)
+        self._popen_obj = None
+        self.popen_kwargs.setdefault('close_fds', True)
+        self._pipe_stdout, self._pipe_stderr = self._create_pipes()
+        self._start()
+
+    def _create_pipes(self):
+        if 'stdout' not in self.popen_kwargs:
+            pipe_stdout = _LogPipe(logging.INFO)
+            self.popen_kwargs['stdout'] = pipe_stdout.write_fd
+        else:
+            pipe_stdout = None
+        if 'stderr' not in self.popen_kwargs:
+            pipe_stderr = _LogPipe(logging.ERROR)
+            self.popen_kwargs['stderr'] = pipe_stderr.write_fd
+        else:
+            pipe_stderr = None
+        return pipe_stdout, pipe_stderr
+
+    def _start(self):
+        if 'stdin' not in self.popen_kwargs:
+            orig_preexec = self.popen_kwargs.get('preexec_fn')
+            self.popen_kwargs['preexec_fn'] = functools.partial(
+                _close_stdin, orig_preexec)
+        self._popen_obj = subprocess.Popen(self.cmd, **self.popen_kwargs)
+        self.name = "%s[%d]" % (self.name, self._popen_obj.pid)
+        if 'stdin' in self.popen_kwargs:
+            self.stdin = self._popen_obj.stdin
+        if self._pipe_stdout:
+            self._pipe_stdout.start(self.name + " stdout")
+        else:
+            self.stdout = self._popen_obj.stdout
+        if self._pipe_stderr:
+            self._pipe_stderr.start(self.name + " stderr")
+        else:
+            self.stderr = self._popen_obj.stderr
+
+    def poll(self):
+        return self._popen_obj.poll()
+
+    def wait(self):
+        return self._popen_obj.wait()
+
+    def terminate(self):
+        return self._popen_obj.terminate()
+
+    def communicate(self):
+        return self._popen_obj.communicate()
+
+
 @contextlib.contextmanager
 def popen(cmd, **kwargs):
     name = kwargs.pop('name', cmd[0])
-    kwargs.setdefault('close_fds', True)
-    if 'stdin' not in kwargs:
-        orig_preexec = kwargs.get('preexec_fn')
-        kwargs['preexec_fn'] = functools.partial(_close_stdin, orig_preexec)
-    if 'stdout' not in kwargs:
-        pipe_stdout = _LogPipe(logging.INFO)
-        kwargs['stdout'] = pipe_stdout.write_fd
-    else:
-        pipe_stdout = None
-    if 'stderr' not in kwargs:
-        pipe_stderr = _LogPipe(logging.ERROR)
-        kwargs['stderr'] = pipe_stderr.write_fd
-    else:
-        pipe_stderr = None
-    proc = subprocess.Popen(cmd, **kwargs)
-    name = "%s[%d]" % (name, proc.pid)
-    LOG.info('Started process %s: %s', name, " ".join(map(pipes.quote, cmd)))
-    if pipe_stdout:
-        pipe_stdout.start(name + " stdout")
-    if pipe_stderr:
-        pipe_stderr.start(name + " stderr")
+    proc = LocalPopen(name, cmd, kwargs)
+    LOG.info('Started process %s: %s', proc.name,
+             " ".join(map(pipes.quote, cmd)))
     try:
         yield proc
     except Exception:
