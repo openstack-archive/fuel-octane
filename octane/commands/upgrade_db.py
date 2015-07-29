010b011a000783bd79f672e7e653a4085956e817
Yuriy Taraday <yorik.sar@gmail.com>
2015-07-29 16:50:54 +0300

    Add upgrade-db command skeleton

---
+++ octane/commands/upgrade_db.py
@@ -0,0 +1,34 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from cliff import command as cmd
+
+
+def upgrade_db(orig_id, seed_id):
+    pass
+
+
+class UpgradeDBCommand(cmd.Command):
+    """Migrate and upgrade state databases data"""
+
+    def get_parser(self, prog_name):
+        parser = super(UpgradeDBCommand, self).get_parser(prog_name)
+        parser.add_argument(
+            'orig_id', type=int, metavar='ORIG_ID',
+            help="ID of original environment")
+        parser.add_argument(
+            'seed_id', type=int, metavar='SEED_ID',
+            help="ID of seed environment")
+        return parser
+
+    def take_action(self, parsed_args):
+        upgrade_db(parsed_args.orig_id, parsed_args.seed_id)
