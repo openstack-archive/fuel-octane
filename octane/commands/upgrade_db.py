f3633dbc3841725d5efc91e17b24d63b8eca8a2a
Yuriy Taraday <yorik.sar@gmail.com>
2015-07-31 15:27:20 +0300

    Add disable_apis method
    Change-Id: I64710bac426577a2a1d8d43f2fd20e947aee89cc

--- octane/commands/upgrade_db.py
+++ octane/commands/upgrade_db.py
@@ -11,6 +11,7 @@
 # under the License.
 
 import os.path
+import re
 import time
 
 from cliff import command as cmd
@@ -47,10 +48,45 @@ def delete_fuel_resources(seed_env):
     )
 
 
+def disable_apis(env):
+    controllers = list(get_controllers(env))
+    maintenance_line = 'backend maintenance'
+    stats_socket_re = re.compile('stats\s+socket\s+/var/lib/haproxy/stats'
+                                 '(?!.*level admin)')
+    mode_tcp_re = re.compile('mode\s+tcp')
+    use_backend_line = '  use_backend maintenance if TRUE'
+    for node in controllers:
+        sftp = ssh.sftp(node)
+        sftp.chdir('/etc/haproxy')
+        with ssh.update_file(sftp, 'haproxy.cfg') as (old, new):
+            found_maint_line = False
+            for line in old:
+                if maintenance_line in line:
+                    found_maint_line = True
+                line = stats_socket_re.sub(r'\g<0> level admin', line)
+                new.write(line)
+            if not found_maint_line:
+                new.write(maintenance_line)
+        sftp.chdir('/etc/haproxy/conf.d')
+        for f in sftp.listdir():
+            with ssh.update_file(sftp, f) as (old, new):
+                contents = old.read()
+                if not mode_tcp_re.search(contents):
+                    raise ssh.DontUpdateException
+                new.write(contents)
+                if not contents.endswith('\n'):
+                    new.write('\n')
+                new.write(use_backend_line)
+    ssh.call(['crm', 'resource', 'restart', 'p_haproxy'], node=controllers[0])
+
+
 def upgrade_db(orig_id, seed_id):
     orig_env = environment_obj.Environment(orig_id)
     seed_env = environment_obj.Environment(seed_id)
     delete_fuel_resources(seed_env)
+    # Wait for Neutron to reconfigure networks
+    time.sleep(7)  # FIXME: Use more deterministic way
+    disable_apis(orig_env)
 
 
 class UpgradeDBCommand(cmd.Command):
