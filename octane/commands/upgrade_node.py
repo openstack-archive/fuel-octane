f2ea167aa237c070b9142e963b8a2176c8592031
Oleg Gelbukh <ogelbukh@mirantis.com>
2015-08-22 18:21:20 +0000

    Check if path exists before create backup path
    Change-Id: Ieedd4be216d13ec864b19c879f343f150500cb3d

--- octane/commands/upgrade_node.py
+++ octane/commands/upgrade_node.py
@@ -98,7 +98,8 @@ class ControllerUpgrade(UpgradeHandler):
                 magic_consts.FUEL_CACHE,
                 "deployment_{0}.orig".format(self.node.data['cluster']),
             )
-            os.makedirs(backup_path)
+            if not os.path.exists(backup_path):
+                os.makedirs(backup_path)
             # Roughly taken from Environment.write_facts_to_dir
             for info in deployment_info:
                 fname = os.path.join(
