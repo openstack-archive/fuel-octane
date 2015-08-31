7d84abfc96066738cb5b200b1f294d748046676b
Oleg Gelbukh <ogelbukh@mirantis.com>
2015-08-31 13:37:25 +0000

    Fix tests for sync networks command
    Change-Id: I9430572c77935e01f18cb06eb1b4ba1b22dbe64d

--- octane/tests/test_sync_networks.py
+++ octane/tests/test_sync_networks.py
@@ -14,10 +14,10 @@
 def test_parser(mocker, octane_app):
     networks = [{'key': 'value'}]
 
-    m1 = mocker.patch('octane.commands.sync_networks.get_original_networks')
+    m1 = mocker.patch('octane.commands.sync_networks.get_env_networks')
     m1.return_value = networks
 
-    m2 = mocker.patch('octane.commands.sync_networks.update_seed_networks')
+    m2 = mocker.patch('octane.commands.sync_networks.update_env_networks')
     octane_app.run(["sync-networks", "1", "2"])
     assert not octane_app.stdout.getvalue()
     assert not octane_app.stderr.getvalue()
