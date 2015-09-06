3585b562eadf4aa3f682dfb4c205dad7243b2afd
Valyavskiy Viacheslav <vvalyavskiy@mirantis.com>
2015-09-06 04:24:13 +0300

    add re-upload network template step

--- octane/commands/upgrade_node.py
+++ octane/commands/upgrade_node.py
@@ -18,6 +18,7 @@ from fuelclient.objects import node as node_obj
 
 from octane.handlers import upgrade as upgrade_handlers
 from octane.util import env as env_util
+from octane.util import subprocess
 
 LOG = logging.getLogger(__name__)
 
@@ -50,6 +51,9 @@ def upgrade_node(env_id, node_ids, isolated=False):
     call_handlers('preupgrade')
     call_handlers('prepare')
     env_util.move_nodes(env, nodes)
+    # re-upload template to make it accessible for new node
+    subprocess.call(["fuel", "--env", str(env_id), "network-template", "-u",
+                     "--dir", "/root"])
     call_handlers('predeploy')
     env_util.deploy_nodes(env, nodes)
     call_handlers('postdeploy')
