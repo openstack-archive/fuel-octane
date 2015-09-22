3a20ac9bb466f5cfd4add60c4ac147b4348c4264
Valyavskiy Viacheslav <vvalyavskiy@mirantis.com>
2015-09-22 23:52:52 +0300

    Disable iscsi actions for clusters without emc plugins
    Change-Id: I2588c6e86a3dc65fc3e6269d8e3927f28d47b0f7

---
+++ octane/util/plugin.py
@@ -0,0 +1,43 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from collections import namedtuple
+
+
+def Enum(*values, **kwargs):
+    names = kwargs.get('names')
+    if names:
+        return namedtuple('Enum', names)(*values)
+    return namedtuple('Enum', values)(*values)
+
+PLUGIN_STATUSES = Enum(
+    'enabled',
+    'available'
+)
+
+
+def plugin_status(env, plugin_name):
+    settings = env.get_settings_data()
+    if __is_available(settings, plugin_name):
+        return PLUGIN_STATUSES.available
+    if __is_enabled(settings, plugin_name):
+        return PLUGIN_STATUSES.enabled
+
+
+def __is_available(settings, plugin_name):
+    if plugin_name in settings['editable']:
+        return True
+    return False
+
+
+def __is_enabled(settings, plugin_name):
+    return settings['editable'][plugin_name]['metadata']['enabled']
