f718ad4daf6b591b85385a803c669382af245336
Valyavskiy Viacheslav <vvalyavskiy@mirantis.com>
2015-09-27 22:54:29 +0300

    Add possibility to switch controlplane back
    Change-Id: I6873d5a1b1d37844f8db5639ffc4c8a9ac0928a2

--- octane/commands/upgrade_controlplane.py
+++ octane/commands/upgrade_controlplane.py
@@ -9,43 +9,13 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-import os
-import yaml
-
 from cliff import command as cmd
 from fuelclient.objects import environment as environment_obj
 
-from octane.helpers import network
 from octane import magic_consts
 from octane.util import env as env_util
 from octane.util import maintenance
 from octane.util import ssh
-
-
-def disconnect_networks(env):
-    controllers = list(env_util.get_controllers(env))
-    for node in controllers:
-        deployment_info = env_util.get_astute_yaml(env, node)
-        network.delete_patch_ports(node, deployment_info)
-
-
-def connect_to_networks(env):
-    deployment_info = []
-    controllers = list(env_util.get_controllers(env))
-    backup_path = os.path.join(magic_consts.FUEL_CACHE,
-                               'deployment_{0}.orig'
-                               .format(env.id))
-    for filename in os.listdir(backup_path):
-        filepath = os.path.join(backup_path, filename)
-        with open(filepath) as info_file:
-            info = yaml.safe_load(info_file)
-            deployment_info.append(info)
-    for node in controllers:
-        for info in deployment_info:
-            if (info['role'] in ('primary-controller', 'controller')
-                    and info['uid'] == str(node.id)):
-                network.delete_overlay_networks(node, info)
-                network.create_patch_ports(node, info)
 
 
 def update_neutron_config(env):
@@ -65,10 +35,12 @@ def upgrade_control_plane(orig_id, seed_id):
     orig_env = environment_obj.Environment(orig_id)
     seed_env = environment_obj.Environment(seed_id)
     update_neutron_config(seed_env)
+    maintenance.stop_corosync_services(orig_env)
     maintenance.start_corosync_services(seed_env)
     maintenance.start_upstart_services(seed_env)
-    disconnect_networks(orig_env)
-    connect_to_networks(seed_env)
+    env_util.disconnect_networks(orig_env)
+    env_util.delete_overlay_networks(seed_env)
+    env_util.connect_to_networks(seed_env)
 
 
 class UpgradeControlPlaneCommand(cmd.Command):
