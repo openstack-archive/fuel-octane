eaef776c1243ebb4b00d36c561260b37738997a8
Oleg Gelbukh <ogelbukh@mirantis.com>
2015-10-06 14:25:35 +0000

    Preserve data on Ceph OSD devices
    Set reinstall_node flag in the deployment information for ceph-osd nodes to
    ensure that Puppet keeps the existing OSD data set.
    
    Closes-bug: 1503424
    Change-Id: I4ddd7b6ba3077ad32961079eebbaf719521bf47a

--- octane/magic_consts.py
+++ octane/magic_consts.py
@@ -18,7 +18,7 @@ PATCHES = [("nailgun", "/usr/lib/python2.6/site-packages/nailgun/extensions"
 # TODO: use pkg_resources for patches
 CWD = os.path.dirname(__file__)  # FIXME
 FUEL_CACHE = "/tmp/octane/deployment"  # TODO: we shouldn't need this
-PUPPET_DIR = "/etc/puppet/2014.2.2-6.1/modules"
+PUPPET_DIR = "/etc/puppet/2015.1.0-7.0/modules"
 BOOTSTRAP_INITRAMFS = "/var/www/nailgun/bootstrap/initramfs.img"
 
 SSH_KEYS = ['/root/.ssh/id_rsa', '/root/.ssh/bootstrap.rsa']
