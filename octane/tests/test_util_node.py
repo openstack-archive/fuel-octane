d3f2618f0a8efdd4db536f87090530b84dc2d0ae
Oleg Gelbukh <ogelbukh@mirantis.com>
2015-12-07 13:02:02 +0000

    Pass FQDN to host_evacuate.sh command
    FQDN is required for nova to identify the host since version 6.1.
    
    Pass FQDN value from node's data hash to command host_evacuate.sh
    if version of the original environment is 6.1 or higher. Pass
    the short name (node-N) of the node otherwise.
    
    Closes-bug: 1523469
    Change-Id: I5be715f4eab445f3cf823526a98a23e7a1749f2a

--- octane/tests/test_util_node.py
+++ octane/tests/test_util_node.py
@@ -20,13 +20,16 @@ from octane.util import ssh
 
 
 NODES = [
-    {'fqdn': 'node-1',
+    {'id': '1',
+     'fqdn': 'node-1.domain.tld',
      'network_data': [{'name': 'management', 'ip': '10.20.0.2'},
                       {'name': 'public', 'ip': '172.167.0.2'}]},
-    {'fqdn': 'node-2',
+    {'id': '2',
+     'fqdn': 'node-2.domain.tld',
      'network_data': [{'name': 'management', 'ip': '10.20.0.3'},
                       {'name': 'public', 'ip': '172.167.0.3'}]},
-    {'fqdn': 'node-3',
+    {'id': '3',
+     'fqdn': 'node-3.domain.tld',
      'network_data': [{'name': 'management', 'ip': '10.20.0.4'},
                       {'name': 'public', 'ip': '172.167.0.4'}]},
 ]
@@ -44,7 +47,7 @@ def test_get_ip(node_data, network_name, expected_ip):
 
 
 def create_node(data):
-    return mock.Mock(data=data, spec_set=['data'])
+    return mock.Mock(data=data, spec_set=['data', 'env'])
 
 
 @pytest.fixture
@@ -63,7 +66,9 @@ def test_get_ips(nodes, network_name, expected_ips):
 
 def test_get_hostnames(nodes):
     hostnames = node_util.get_hostnames(nodes)
-    assert hostnames == ['node-1', 'node-2', 'node-3']
+    assert hostnames == ['node-1.domain.tld',
+                         'node-2.domain.tld',
+                         'node-3.domain.tld']
 
 
 def test_tar_files(node, mock_ssh_popen, mock_open):
@@ -141,3 +146,14 @@ def test_remove_compute_upgrade_levels(mocker, node, content,
                                        expected_content):
     with _check_upgrade_levels(mocker, node, content, expected_content):
         node_util.remove_compute_upgrade_levels(node)
+
+
+@pytest.mark.parametrize('node_data,fuel_version,expected_name', [
+    (NODES[0], '6.0', 'node-1'),
+    (NODES[0], '6.1', 'node-1.domain.tld'),
+])
+def test_get_nova_node_handle(mocker, node_data, fuel_version, expected_name):
+    node = create_node(node_data)
+    node.env.data.get.return_value = fuel_version
+    name = node_util.get_nova_node_handle(node)
+    assert name == expected_name
