4b7d3b27b933c5261df5a5ac9730ee9f11a93836
Sergey Abramov <sabramov@mirantis.com>
2016-01-13 21:08:34 +0300

    Add fuel admin node backup tool
    Blueprint: create-standalone-script-to-backup-masternode
    Change-Id: I8a4e60c5cc3c212b7e0b145b109bc4c498ea6641
    (cherry picked from commit 91a37d57e66ec822fc91610f3b282b320685194e)

---
+++ octane/handlers/backup_restore/postgres.py
@@ -0,0 +1,39 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import six
+
+from octane.handlers.backup_restore import base
+
+
+class PostgresArchivatorMeta(type):
+
+    def __init__(cls, name, bases, attr):
+        super(PostgresArchivatorMeta, cls).__init__(name, bases, attr)
+        cls.container = "postgres"
+        if cls.db is not None and cls.cmd is None:
+            cls.cmd = ["sudo", "-u", "postgres", "pg_dump", "-C", cls.db]
+        if cls.db is not None and cls.filename is None:
+            cls.filename = "postgres/{0}.sql".format(cls.db)
+
+
+@six.add_metaclass(PostgresArchivatorMeta)
+class PostgresArchivator(base.CmdArchivator):
+    db = None
+
+
+class NailgunArchivator(PostgresArchivator):
+    db = "nailgun"
+
+
+class KeystoneArchivator(PostgresArchivator):
+    db = "keystone"
