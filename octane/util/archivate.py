fa7be9d0ef20d68b18eac94dfd422eacb37092d8
Sergey Abramov <sabramov@mirantis.com>
2016-01-13 21:08:34 +0300

    Add fuel admin node backup tool
    Blueprint: create-standalone-script-to-backup-masternode
    Change-Id: I8a4e60c5cc3c212b7e0b145b109bc4c498ea6641

---
+++ octane/util/archivate.py
@@ -0,0 +1,57 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import io
+import os
+import tarfile
+
+from octane.util import docker
+from octane.util import subprocess
+
+
+def archive_dirs(archive, src_path, tag_name):
+    """Archive all dirs from src path
+
+    saved all dirs from src path to archive with
+    name like {tag_name}/{dir_name}
+
+    :param archive: tar archive for writing or appending
+    :param src_path: path for backuping dir
+    :param tag_name: start part name for current backup dir
+    """
+    if not os.path.exists(src_path):
+        return
+    for directory in os.listdir(src_path):
+        dirpath = os.path.join(src_path, directory)
+        if not os.path.isdir(dirpath):
+            continue
+        if os.path.islink(dirpath):
+            continue
+        archive.add(dirpath, "{0}/{1}".format(tag_name, directory))
+
+
+def archivate_container_cmd_output(archive, container, cmd, filename):
+    """archivate container command aoutput
+
+    save command output runnning in container to archive with current tag
+    :param archive: tar archive for writing or appending
+    :param container: container name
+    :param cmd: sequence of program arguments
+    :param filename: name for saving output in archive
+    """
+    info = tarfile.TarInfo(filename)
+    dump = io.BytesIO()
+    data, _ = docker.run_in_container(container, cmd, stdout=subprocess.PIPE)
+    info.size = len(data)
+    dump.write(data)
+    dump.seek(0)
+    archive.addfile(info, dump)
