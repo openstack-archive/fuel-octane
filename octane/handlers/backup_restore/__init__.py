ecf4f83c7c8dbc97d8e4f9185071911702419eb2
Sergey Abramov <sabramov@mirantis.com>
2016-01-14 14:15:17 +0300

    Add restore admin node tool
    Change-Id: I654d46af79a436e1657fb0e4348f41a1cb62f24d

--- octane/handlers/backup_restore/__init__.py
+++ octane/handlers/backup_restore/__init__.py
@@ -10,6 +10,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from octane.handlers.backup_restore import actions
 from octane.handlers.backup_restore import astute
 from octane.handlers.backup_restore import cobbler
 from octane.handlers.backup_restore import fuel_keys
@@ -30,4 +31,8 @@ ARCHIVATORS = [
     puppet.PuppetArchivator,
     ssh.SshArchivator,
     version.VersionArchivator,
+]
+
+POST_RESTORE_ACTIONS = [
+    actions.add_releases,
 ]
