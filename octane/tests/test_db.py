3815f41a2b47aef7a52b1179797357ff03bd10ae
Mykola Yakovliev <myakovliev@mirantis.com>
2016-01-21 12:35:37 -0700

    Migrate Database from custom role
    In several installations, database assigned to custom role.
    This patch allows to specify rolename to work with.
    
    Change-Id: Iddef017e4c0fc0c43af6de5a05ef4b2e8ac5829d

--- octane/tests/test_db.py
+++ octane/tests/test_db.py
@@ -14,8 +14,19 @@ import io
 
 import mock
 
+from octane.commands import upgrade_db
 from octane.util import db
+from octane.util import env
 from octane.util import ssh
+
+
+class FakeEnv(object):
+    data = {'id': 'some_id'}
+
+
+def fake_nodes():
+    for node in ['node1', 'node2']:
+        yield node
 
 
 def test_mysqldump_from_env(mocker, mock_open, mock_subprocess, mock_ssh_popen,
@@ -25,13 +36,13 @@ def test_mysqldump_from_env(mocker, mock_open, mock_subprocess, mock_ssh_popen,
 
     mock_open.return_value.write.side_effect = buf.write
 
-    get_one_controller = mocker.patch('octane.util.env.get_one_controller')
-    get_one_controller.return_value = node
+    get_one_node_of = mocker.patch('octane.util.env.get_one_node_of')
+    get_one_node_of.return_value = node
 
     proc = mock_ssh_popen.return_value.__enter__.return_value
     proc.stdout = io.BytesIO(test_contents)
 
-    db.mysqldump_from_env('env', ['db1'], 'filename')
+    db.mysqldump_from_env('env', 'controller', ['db1'], 'filename')
 
     assert not mock_subprocess.called
     mock_ssh_popen.assert_called_once_with(
@@ -47,13 +58,13 @@ def test_mysqldump_restore_to_env(mocker, mock_open, mock_subprocess,
 
     mock_open.return_value = io.BytesIO(test_contents)
 
-    get_one_controller = mocker.patch('octane.util.env.get_one_controller')
-    get_one_controller.return_value = node
+    get_one_node_of = mocker.patch('octane.util.env.get_one_node_of')
+    get_one_node_of.return_value = node
 
     proc = mock_ssh_popen.return_value.__enter__.return_value
     proc.stdin.write.side_effect = buf.write
 
-    db.mysqldump_restore_to_env('env', 'filename')
+    db.mysqldump_restore_to_env('env', 'controller', 'filename')
 
     assert not mock_subprocess.called
     mock_ssh_popen.assert_called_once_with(
@@ -73,3 +84,47 @@ def test_db_sync(mocker, node, mock_subprocess, mock_ssh_call):
                for call in mock_ssh_call.call_args_list)
     assert all(call[1]['node'] == node
                for call in mock_ssh_call.call_args_list)
+
+
+def test_upgrade_db(mocker, node):
+    FAKE_ROLE_NAME = 'some_name'
+
+    mocker.patch('fuelclient.objects.environment.Environment')
+
+    mysqldump_restore = mocker.patch('octane.util.db.mysqldump_restore_to_env')
+    delete_fuel_res = mocker.patch('octane.util.env.delete_fuel_resources')
+    mysqldump_from_env = mocker.patch('octane.util.db.mysqldump_from_env')
+    disable_apis = mocker.patch('octane.util.maintenance.disable_apis')
+    get_databases = mocker.patch('octane.util.db.get_databases')
+    db_sync = mocker.patch('octane.util.db.db_sync')
+    copy_backup = mocker.patch('shutil.copy')
+
+    stop_cor = mocker.patch('octane.util.maintenance.stop_corosync_services')
+    stop_ups = mocker.patch('octane.util.maintenance.stop_upstart_services')
+
+    upgrade_db.upgrade_db('1', '2', FAKE_ROLE_NAME)
+
+    copy_backup.assert_called_once_with('/tmp/dbs.original.sql.gz', mock.ANY)
+
+    mysqldump_from_env.assert_called_once_with(
+        mock.ANY, FAKE_ROLE_NAME, mock.ANY, '/tmp/dbs.original.sql.gz')
+
+    mysqldump_restore.assert_called_once_with(
+        mock.ANY, FAKE_ROLE_NAME, '/tmp/dbs.original.sql.gz')
+
+    delete_fuel_res.assert_called_once_with(mock.ANY)
+    get_databases.assert_called_once_with(mock.ANY)
+    disable_apis.assert_called_once_with(mock.ANY)
+    stop_cor.assert_called_once_with(mock.ANY)
+    stop_ups.assert_called_once_with(mock.ANY)
+    db_sync.assert_called_once_with(mock.ANY)
+
+
+def test_get_one_node_of(mocker):
+    get_nodes = mocker.patch('octane.util.env.get_nodes')
+    get_nodes.return_value = fake_nodes()
+
+    fake_env = FakeEnv()
+
+    node = env.get_one_node_of(fake_env, 'controller')
+    assert node == 'node1'
