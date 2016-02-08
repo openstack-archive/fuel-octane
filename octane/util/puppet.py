772287ad73dc7a6817132ce0ac7496843abad189
Oleg Gelbukh <ogelbukh@mirantis.com>
2016-02-08 12:51:10 +0000

    Run 'puppet apply' command at host
    Run command 'puppet apply' in host system at Fuel Admin node
    immediately after the /etc/fuel/astute.yaml file is updates
    with values from backup.
    
    Change-Id: Ibd59fb6b0d2d37f708e8d4c48c99a92d4e9ceef9

---
+++ octane/util/puppet.py
@@ -0,0 +1,34 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import logging
+import os.path
+
+from octane import magic_consts
+from octane.util import subprocess
+
+LOG = logging.getLogger(__name__)
+
+
+def apply_host():
+    cmd = ['puppet', 'apply', '-d', '-v', '--detailed-exitcodes']
+    path = os.path.join(magic_consts.PUPPET_DIR,
+                        'nailgun',
+                        'examples',
+                        'host-only.pp')
+    cmd.append(path)
+    try:
+        subprocess.call(cmd)
+    except subprocess.CalledProcessError as exc:
+        LOG.error("Cannot apply Puppet state on host: %s",
+                  exc.message)
+        raise
