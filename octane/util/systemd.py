6085068eeb5cff0f089c3b733bdb4997d7d08599
Oleg Gelbukh <ogelbukh@mirantis.com>
2016-02-08 14:25:33 +0000

    Use systemd commands to start/stop service containers
    In 8.0, docker containers that run services are controlled
    by systemd as docker-<name>.service.
    
    Add systemd.py module for actions on systemd units using
    systemctl: start, stop service and configure timeout for
    start action.
    
    In systemd, default timeout for starting/stopping operation
    is 90s. This is insufficient for operations on Fuel contianers
    as they run Puppet manifests and/or database sync before the
    service actually started.
    
    Override default value for the timeout via configuration file
    in /etc/systemd/user.conf.d directory.
    
    Replace dockerctl-based operations on containers with
    systemd-based ones.
    
    Change-Id: I0fd5879c4817092400ced1b48b6ac9bd1613d07e

---
+++ octane/util/systemd.py
@@ -0,0 +1,55 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import logging
+import os
+import os.path
+
+from contextlib import contextmanager
+from octane.util import subprocess
+
+
+LOG = logging.getLogger(__name__)
+TIMEOUT_FILE = "/etc/systemd/user.conf.d/99-octane-timeout.conf"
+TIMEOUT_CONF = ("[Service]\n"
+                "TimeoutStartSec={0}\n")
+
+
+@contextmanager
+def set_systemctl_start_timeout(timeout):
+    confdir = os.path.dirname(TIMEOUT_FILE)
+    if not os.path.isdir(confdir):
+        os.makedirs(confdir)
+    with open(os.path.join(TIMEOUT_FILE), 'w') as f:
+        f.write(TIMEOUT_CONF.format(timeout))
+    LOG.info("Set timeout for systemd service start to %s", timeout)
+    try:
+        yield
+    finally:
+        os.unlink(TIMEOUT_FILE)
+        LOG.info("Unset systemd timeout override for service start")
+
+
+def _container_action(service, action):
+    subprocess.call(["systemctl",
+                     action,
+                     "docker-{0}.service".format(service)])
+
+
+def stop_container(service):
+    _container_action(service, "stop")
+    LOG.info("Container for service %s stopped", service)
+
+
+def start_container(service):
+    _container_action(service, "start")
+    LOG.info("Container for service %s started", service)
