baefbee34b6acd2ce3402eb09b9805cb0b7c3084
Sergey Abramov <sabramov@mirantis.com>
2016-02-10 12:10:20 +0300

    Encrypt and Decrypt backup
    Change-Id: I1fa402243a0819b1fa8d71b7c486ef783dca80dc

--- octane/tests/test_backup.py
+++ octane/tests/test_backup.py
@@ -10,23 +10,24 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 import pytest
-import sys
 
 from octane.commands import backup
 from octane.handlers import backup_restore
 
 
-@pytest.mark.parametrize("cmd,archivators", [
-    ("fuel-backup", backup_restore.ARCHIVATORS),
-    ("fuel-repo-backup", backup_restore.REPO_ARCHIVATORS),
+@pytest.mark.parametrize("cmd,archivators,password", [
+    ("fuel-backup", backup_restore.ARCHIVATORS, "password"),
+    ("fuel-repo-backup", backup_restore.REPO_ARCHIVATORS, None),
 ])
 @pytest.mark.parametrize("path", [None, "backup_file"])
-def test_parser_empty(mocker, octane_app, cmd, archivators, path):
+def test_parser_empty(mocker, octane_app, cmd, archivators, path, password):
     m1 = mocker.patch('octane.commands.backup.backup')
     m1.return_value = 2
     params = [cmd]
     if path:
         params += ["--to", path]
+    if password:
+        params += ["--password", password]
     try:
         octane_app.run(params)
     except AssertionError:
@@ -34,7 +35,7 @@ def test_parser_empty(mocker, octane_app, cmd, archivators, path):
     else:
         assert not octane_app.stdout.getvalue()
         assert not octane_app.stderr.getvalue()
-        m1.assert_called_once_with(path, archivators)
+        m1.assert_called_once_with(path, archivators, password)
 
 
 @pytest.mark.parametrize("path,mode", [
@@ -44,7 +45,9 @@ def test_parser_empty(mocker, octane_app, cmd, archivators, path):
     ("path.hz2", "w|"),
 ])
 @pytest.mark.parametrize("empty", [True, False])
-def test_backup_admin_node_backup_file(mocker, path, mode, empty):
+@pytest.mark.parametrize("password", [None, "password"])
+def test_backup_admin_node_backup_file(
+        mocker, mock_open, path, mode, empty, password):
     manager = mocker.Mock()
     tar_obj = mocker.patch("tarfile.open")
     if empty:
@@ -53,23 +56,27 @@ def test_backup_admin_node_backup_file(mocker, path, mode, empty):
     unlink_mock = mocker.patch("os.unlink")
     mocker.patch("os.path.isfile", return_value=True)
     move_mock = mocker.patch("shutil.move")
+    enc_mock = mocker.patch("octane.util.encryption.encrypt_io")
     try:
-        backup.backup(path, [manager])
+        backup.backup(path, [manager], password)
     except AssertionError as exc:
         assert "backup is empty" == exc.msg and empty
     manager.assert_called_once_with(tar_obj.return_value)
     manager.return_value.backup.assert_called_once_with()
-    if path is not None:
-        tmp_file.assert_called_once_with(delete=False)
-        tar_obj.assert_called_once_with(
-            fileobj=tmp_file.return_value, mode=mode)
-        unlink_mock.assert_called_once_with(tmp_file.return_value.name)
-        if empty:
-            assert not move_mock.called
-        else:
-            move_mock.assert_called_once_with(tmp_file.return_value.name, path)
-    else:
-        tar_obj.assert_called_once_with(fileobj=sys.stdout, mode=mode)
-        assert not tmp_file.called
-        assert not unlink_mock.called
+    tmp_file.assert_called_once_with(delete=False)
+    tar_obj.assert_called_once_with(
+        fileobj=tmp_file.return_value, mode=mode)
+    unlink_mock.assert_called_once_with(tmp_file.return_value.name)
+    if empty:
         assert not move_mock.called
+        assert not enc_mock.called
+        assert not mock_open.called
+    elif password:
+        assert mock_open.called
+        enc_mock.assert_called_once_with(
+            tmp_file.return_value, mock_open.return_value, password)
+        assert not move_mock.called
+    else:
+        move_mock.assert_called_once_with(tmp_file.return_value.name, path)
+        assert not enc_mock.called
+        assert not mock_open.called
