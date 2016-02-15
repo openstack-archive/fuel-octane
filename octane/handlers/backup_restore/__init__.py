528b6d22b9c08fea58bd4de9057d18b4fdd67be7
Sergey Abramov <sabramov@mirantis.com>
2016-02-15 13:56:42 +0300

    Add backup and restore repo action
    Change-Id: I0399ab3748283027aa11b63e92385f8243ac572e

--- octane/handlers/backup_restore/__init__.py
+++ octane/handlers/backup_restore/__init__.py
@@ -16,6 +16,7 @@ from octane.handlers.backup_restore import astute
 from octane.handlers.backup_restore import cobbler
 from octane.handlers.backup_restore import fuel_keys
 from octane.handlers.backup_restore import fuel_uuid
+from octane.handlers.backup_restore import mirrors
 from octane.handlers.backup_restore import nailgun_plugins
 from octane.handlers.backup_restore import postgres
 from octane.handlers.backup_restore import puppet
@@ -35,6 +36,11 @@ ARCHIVATORS = [
     ssh.SshArchivator,
     version.VersionArchivator,
     nailgun_plugins.NailgunPluginsArchivator,
+]
+
+REPO_ARCHIVATORS = [
+    mirrors.MirrorsBackup,
+    mirrors.RepoBackup,
 ]
 
 
