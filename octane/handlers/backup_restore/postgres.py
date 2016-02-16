578232b9ec55adc8569b7ff49581a940ccfde838
Oleg Gelbukh <ogelbukh@mirantis.com>
2016-02-16 11:02:20 +0000

    Start container before systemd unit for nailgun and keystone
    Dockerctl improperly detects readyness of docker container. Thus,
    we need to start the container before corresponding systemd unit
    (service).
    
    Run container first ensures that the systemd unit properly finishes
    its work.
    
    Add 5 seconds delay between starts as a workaround for race condition
    in dockerctl's check_ready function (bug #1545825).
    
    Change-Id: Id34986f42dd9944969475d5e0c1bec9814746b5f
    Related-bug: 1545825

--- octane/handlers/backup_restore/postgres.py
+++ octane/handlers/backup_restore/postgres.py
@@ -14,6 +14,7 @@ import json
 import logging
 import requests
 import six
+import time
 import urlparse
 import yaml
 
@@ -58,10 +59,11 @@ class PostgresArchivator(base.CmdArchivator):
                                  ["sudo", "-u", "postgres", "psql"],
                                  stdin=subprocess.PIPE) as process:
             process.stdin.write(dump.read())
+        docker.start_container(self.db)
+        time.sleep(5)
         subprocess.call([
             "systemctl", "start", "docker-{0}.service".format(self.db)
         ])
-        docker.start_container(self.db)
 
 
 class NailgunArchivator(PostgresArchivator):
