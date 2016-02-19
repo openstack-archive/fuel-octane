c4bf442287b35419e85c5c300c54917e94e2aec0
Sergey Abramov <sabramov@mirantis.com>
2016-02-19 15:47:45 +0300

    Fix creation empty file if exception on backup
    Change-Id: I237ed62003c811a9844c4078dfce89ba715810a7
    (cherry picked from commit 4ee82368b2ca1ebb41064dc2f951385adf04e2c6)

--- octane/tests/test_backup.py
+++ octane/tests/test_backup.py
@@ -9,6 +9,7 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+
 import pytest
 import sys
 
@@ -43,13 +44,34 @@ def test_parser_empty(mocker, octane_app, cmd, archivators, path):
     ("path.bz2", "w|bz2"),
     ("path.hz2", "w|"),
 ])
-def test_backup_admin_node_backup_file(mocker, path, mode):
+@pytest.mark.parametrize("empty", [True, False])
+def test_backup_admin_node_backup_file(mocker, path, mode, empty):
     manager = mocker.Mock()
     tar_obj = mocker.patch("tarfile.open")
-    backup.backup(path, [manager])
+    if empty:
+        tar_obj.return_value.getmembers.return_value = []
+    tmp_file = mocker.patch("tempfile.NamedTemporaryFile")
+    tmp_file.return_value.__enter__.return_value = tmp_file
+    move_mock = mocker.patch("shutil.move")
+    dir_path = "/abs"
+    abs_path = "{0}/{1}".format(dir_path, path)
+    os_abs_path_mock = mocker.patch("os.path.abspath", return_value=abs_path)
+    if empty:
+        with pytest.raises(Exception) as exc:
+            backup.backup(path, [manager])
+        assert "Nothing to backup" == exc.value.message
+    else:
+        backup.backup(path, [manager])
+    os_abs_path_mock.assert_called_once_with(path)
     manager.assert_called_once_with(tar_obj.return_value)
     manager.return_value.backup.assert_called_once_with()
+    tmp_file.assert_called_once_with(dir=dir_path, prefix=".{0}.".format(path))
     if path is not None:
-        tar_obj.assert_called_once_with(path, mode)
+        tar_obj.assert_called_once_with(fileobj=tmp_file, mode=mode)
+        if empty:
+            assert not move_mock.called
+        else:
+            move_mock.assert_called_once_with(tmp_file.name, abs_path)
     else:
         tar_obj.assert_called_once_with(fileobj=sys.stdout, mode=mode)
+        assert not move_mock.called
