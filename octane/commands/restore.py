1afa17899ac07e2839658fd5af4d6e35318f1f2a
Sergey Abramov <sabramov@mirantis.com>
2016-03-01 13:45:00 +0300

    Tool for update authorized_keys in centos bootstrap image
    Change-Id: Ib489b49d40a08df9981793efc1b754b840b4f3ad
    Closes-bug: 1551651

--- octane/commands/restore.py
+++ octane/commands/restore.py
@@ -13,11 +13,15 @@
 import contextlib
 import logging
 import os
+import shutil
 import tarfile
+import tempfile
 
 from cliff import command
 
 from octane.handlers import backup_restore
+from octane import magic_consts
+from octane.util import subprocess
 
 LOG = logging.getLogger(__name__)
 
@@ -61,3 +65,28 @@ class RestoreCommand(BaseRestoreCommand):
 class RestoreRepoCommand(BaseRestoreCommand):
 
     archivators = backup_restore.REPO_ARCHIVATORS
+
+
+class UpdateBootstrapCentos(command.Command):
+
+    def take_action(self, parsed_args):
+        tmp_dir = tempfile.mkdtemp()
+        try:
+            with subprocess.popen(
+                    ["gunzip", "-c", magic_consts.BOOTSTRAP_INITRAMFS],
+                    stdout=subprocess.PIPE) as proc:
+                subprocess.call(
+                    ["cpio", "-id"], stdin=proc.stdout, cwd=tmp_dir)
+            shutil.copy(
+                "/root/.ssh/authorized_keys",
+                os.path.join(tmp_dir, "root/.ssh/authorized_keys"))
+            with tempfile.NamedTemporaryFile() as new_img:
+                subprocess.call(
+                    ["find | grep -v '^\.$' |cpio --format newc -o | gzip -c"],
+                    shell=True,
+                    stdout=new_img,
+                    cwd=tmp_dir)
+                shutil.move(new_img.name, magic_consts.BOOTSTRAP_INITRAMFS)
+                new_img.delete = False
+        finally:
+            shutil.rmtree(tmp_dir)
