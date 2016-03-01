8a8247f6a9d1fa989d91e48e4863956173074925
Sergey Abramov <sabramov@mirantis.com>
2016-03-01 13:45:00 +0300

    Tool for update authorized_keys in centos bootstrap image
    Change-Id: Ib489b49d40a08df9981793efc1b754b840b4f3ad
    Closes-bug: 1551651
    (cherry picked from commit 0356fe7a227c2ea01651d2a5234e234212dae79a)

---
+++ octane/commands/update_bootstrap.py
@@ -0,0 +1,33 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import os
+import shutil
+
+from cliff import command
+
+from octane import magic_consts
+from octane.util import archivate
+
+
+def update_centos_bootstrap():
+    with archivate.update_cpio(magic_consts.BOOTSTRAP_INITRAMFS) as tmp_dir:
+        shutil.copy(
+            "/root/.ssh/authorized_keys",
+            os.path.join(tmp_dir, "root/.ssh/authorized_keys"))
+
+
+class UpdateCentos(command.Command):
+    """Update Centos bootstrap image"""
+
+    def take_action(self, parsed_args):
+        update_centos_bootstrap()
