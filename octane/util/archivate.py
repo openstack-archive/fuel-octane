cf61971bea01ee93ce54e3e51ef646480f6f0be1
Sergey Abramov <sabramov@mirantis.com>
2016-03-02 18:05:01 +0300

    Unpack bootstrap img in context manager
    Change-Id: I386ea151c5675f93da4359fadd8adaa6ba890e67

--- octane/util/archivate.py
+++ octane/util/archivate.py
@@ -10,9 +10,12 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import contextlib
 import io
 import os
+import shutil
 import tarfile
+import tempfile
 
 from octane.util import docker
 from octane.util import subprocess
@@ -63,3 +66,37 @@ def filter_members(archive, dir_name):
     for member in archive:
         if member.isfile() and member.name.startswith(dir_name):
             yield member
+
+
+@contextlib.contextmanager
+def update_cpio(img_path, dir_path=None):
+    tmp_dir = tempfile.mkdtemp(dir=dir_path)
+    try:
+        with subprocess.popen(
+                ["gunzip", "-c", img_path],
+                stdout=subprocess.PIPE) as proc:
+            subprocess.call(
+                ["cpio", "-id"], stdin=proc.stdout, cwd=tmp_dir)
+        yield tmp_dir
+        tmp_dir_len = len(tmp_dir)
+        with tempfile.NamedTemporaryFile(dir=dir_path) as new_img:
+            with subprocess.popen(
+                    ["cpio", "--format", "newc", "-o"],
+                    stdin=subprocess.PIPE,
+                    stdout=subprocess.PIPE,
+                    cwd=tmp_dir) as cpio:
+                with subprocess.popen(
+                        ["gzip", "-c"],
+                        stdin=cpio.stdout,
+                        stdout=new_img,
+                        cwd=tmp_dir):
+                    for path, _, files in os.walk(tmp_dir):
+                        for f_name in files:
+                            f_path = os.path.join(path, f_name)
+                            f_path = ".{0}".format(f_path[tmp_dir_len:])
+                            cpio.stdin.write("{0}\n".format(f_path))
+                    cpio.stdin.close()
+            shutil.move(new_img.name, img_path)
+            new_img.delete = False
+    finally:
+        shutil.rmtree(tmp_dir)
