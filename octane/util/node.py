23d1b75e365e655239dd12beddcfcabd14be8cfd
Sergey Ryabin <sryabin@mirantis.com>
2016-03-04 00:20:07 +0300

    Stopping the neutron l3 agent before controller upgrade procedure
    Graceful shutdown of l3 agent with migration of routers
    served by it allows to minimize network disruption for
    tenant VMs during the upgrade procedure.
    
    Otherwise the abnormal shutdown of l3 agent causes routers
    to be unavailable for 10s of minutes upon upgrade of the
    first controller from the environment.
    
    Change-Id: I1360bba260ffc4b16565a3d97cfa708bbf6bcdec
    Closes-Bug: #1543972

--- octane/util/node.py
+++ octane/util/node.py
@@ -11,13 +11,17 @@
 # under the License.
 
 import functools
+import json
 import logging
+import pipes
 import shutil
 import socket
+import subprocess
 import sys
 import time
 
 from distutils import version
+from octane import magic_consts
 from octane.util import ssh
 
 LOG = logging.getLogger(__name__)
@@ -158,3 +162,57 @@ def is_live_migration_supported(node):
                     and "VIR_MIGRATE_LIVE" in line:
                 return True
     return False
+
+
+def get_agent_data(node, router_id):
+    cmd = "neutron l3-agent-list-hosting-router {0} -f json".format(router_id)
+    stdout, _ = call_with_openrc(cmd, node)
+    try:
+        agents = json.loads(stdout)
+    except ValueError:
+        LOG.error("Invalid agents data for router %s", router_id)
+        raise Exception("Invalid agents data for router {0}"
+                        .format(router_id))
+    return agents[0]
+
+
+def ban_l3_agent(node):
+    ssh.call(
+        ['pcs', 'resource', 'ban', 'p_neutron-l3-agent', node.data['fqdn']],
+        node=node)
+
+
+def wait_for_router_migration(node, router_id):
+    for i in range(0, 30):
+        agent = get_agent_data(node, router_id)
+        if node.data['fqdn'] != agent['host']:
+            if agent['alive'] == magic_consts.OPENSTACK_SERVICE_STATE_UP:
+                return
+        time.sleep(3)
+    raise Exception("Timeout for router {0} migration".format(router_id))
+
+
+def router_list(node):
+    node_routers = []
+    cmd = "neutron router-list -f json".split()
+    output, _ = call_with_openrc(cmd, node)
+    try:
+        env_routers = json.loads(output)
+    except ValueError:
+        raise Exception("Invalid data from router list")
+
+    for env_router in env_routers:
+        router_id = env_router['id']
+        agent = get_agent_data(node, router_id)
+
+        if agent['host'] == node.data['fqdn']:
+            node_routers.append(router_id)
+
+    return node_routers
+
+
+def call_with_openrc(cmd, node):
+    cmd_string = " ".join(map(pipes.quote, cmd))
+    return ssh.call(["/bin/bash", "-c", ". /root/openrc &&" + cmd_string],
+                    stdout=subprocess.PIPE,
+                    node=node)
