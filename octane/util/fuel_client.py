865985bddd7bc1c9c731d79e5fd2fdf0e89f98a0
Ilya Kharin <akscram@gmail.com>
2016-04-08 22:28:37 -0500

    Fix keystone context for fuelclient==9.0.0
    In the 9.0 release fuelclient was changed and the previously developed
    approach does not work anymore. This change also includes modification
    of test for the 8.0.0 version of the client.
    
    Change-Id: Id1813b2365729c630a1aae0f6dd6a44e666d323e

--- octane/util/fuel_client.py
+++ octane/util/fuel_client.py
@@ -12,11 +12,13 @@
 
 import contextlib
 
+import fuelclient
 from fuelclient import client
+from fuelclient import fuelclient_settings
 
 
 @contextlib.contextmanager
-def set_auth_context(auth_context):
+def set_auth_context_80(auth_context):
     old_credentials = (client.APIClient.user, client.APIClient.password)
     client.APIClient.user = auth_context.user
     client.APIClient.password = auth_context.password
@@ -26,3 +28,27 @@ def set_auth_context(auth_context):
     finally:
         (client.APIClient.user, client.APIClient.password) = old_credentials
         client.APIClient._session = client.APIClient._keystone_client = None
+
+
+@contextlib.contextmanager
+def set_auth_context_90(auth_context):
+    settings = fuelclient_settings.get_settings()
+    config = settings.config
+    old_credentials = (settings.OS_USERNAME, settings.OS_PASSWORD)
+    config['OS_USERNAME'] = auth_context.user
+    config['OS_PASSWORD'] = auth_context.password
+    client.APIClient._session = client.APIClient._keystone_client = None
+    try:
+        yield
+    finally:
+        (config['OS_USERNAME'], config['OS_PASSWORD']) = old_credentials
+        client.APIClient._session = client.APIClient._keystone_client = None
+
+
+# NOTE(akscram): The 9.0.0 release for fuelclient is not yet available
+# on PyPI but to test it on master nodes with the 9.0 release some
+# workaround is needed.
+if fuelclient.__version__ == "8.0.0":
+    set_auth_context = set_auth_context_80
+else:
+    set_auth_context = set_auth_context_90
