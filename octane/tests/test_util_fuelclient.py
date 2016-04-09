a4bb2cf0055a9c8b08b1036a172fbf15aae698dc
Ilya Kharin <akscram@gmail.com>
2016-04-08 22:28:37 -0500

    Fix keystone context for fuelclient==9.0.0
    In the 9.0 release fuelclient was changed and the previously developed
    approach does not work anymore.
    
    Change-Id: Id1813b2365729c630a1aae0f6dd6a44e666d323e

--- octane/tests/test_util_fuelclient.py
+++ octane/tests/test_util_fuelclient.py
@@ -10,40 +10,93 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from fuelclient import client
-from fuelclient import fuelclient_settings
+import mock
+import pytest
 
 from octane.util import fuel_client
 
 
-def test_simple_overwrite(mocker):
+class MockFuelClient80(mock.Mock):
+    def initial_credentials(self, user, password):
+        self.user = user
+        self.password = password
 
-    class TestContext(object):
+    def assert_credentials(self, user, password):
+        assert self.user == user
+        assert self.password == password
 
-        user = "test user"
-        password = "test password"
 
-    conf = fuelclient_settings.get_settings()
+class MockFuelClient90(mock.Mock):
+    def initial_credentials(self, user, password):
+        self._config['OS_USERNAME'] = user
+        self._config['OS_PASSWORD'] = password
 
-    client_val = "Not empty val"
+    def assert_credentials(self, user, password):
+        assert self._config['OS_USERNAME'] == user
+        assert self._config['OS_PASSWORD'] == password
 
-    assert conf.KEYSTONE_USER == client.APIClient.user
-    assert conf.KEYSTONE_PASS == client.APIClient.password
-    assert client.APIClient._session is None
-    assert client.APIClient._keystone_client is None
 
-    client.APIClient._session = client.APIClient._keystone_client = client_val
+class FakeSettings(object):
+    def __init__(self):
+        self.config = {}
 
-    with fuel_client.set_auth_context(TestContext()):
-        assert TestContext.user == client.APIClient.user
-        assert TestContext.password == client.APIClient.password
-        assert client.APIClient._session is None
-        assert client.APIClient._keystone_client is None
+    @property
+    def OS_USERNAME(self):
+        return self.config["OS_USERNAME"]
 
-        client.APIClient._session = client_val
-        client.APIClient._keystone_client = client_val
+    @property
+    def OS_PASSWORD(self):
+        return self.config["OS_PASSWORD"]
 
-    assert conf.KEYSTONE_USER == client.APIClient.user
-    assert conf.KEYSTONE_PASS == client.APIClient.password
-    assert client.APIClient._session is None
-    assert client.APIClient._keystone_client is None
+
+@pytest.fixture
+def mock_fuelclient_80(mocker):
+    client = mocker.patch("fuelclient.client.APIClient",
+                          new_callable=MockFuelClient80)
+    client.mock_add_spec(
+        ["user", "password", "_session", "_keystone_client"],
+        spec_set=True,
+    )
+    return client
+
+
+@pytest.fixture
+def mock_fuelclient_90(mocker):
+    get_settings = mocker.patch("fuelclient.fuelclient_settings.get_settings")
+    get_settings.return_value = settings = FakeSettings()
+    client = mocker.patch("fuelclient.client.APIClient",
+                          new_callable=MockFuelClient90)
+    client.mock_add_spec(
+        ["user", "password", "_session", "_keystone_client", "_config"],
+        spec_set=True,
+    )
+    client._config = settings.config
+    return client
+
+
+# NOTE(akscram): It's not possible to use fixtures in parametrized tests
+# as parameters and I use them as common functions. For more information
+# take a look on this: https://github.com/pytest-dev/pytest/issues/349
+@pytest.mark.parametrize(("auth_context", "fuelclient_fixture"), [
+    (fuel_client.set_auth_context_80, mock_fuelclient_80),
+    (fuel_client.set_auth_context_90, mock_fuelclient_90),
+])
+def test_simple_overwrite(mocker, auth_context, fuelclient_fixture):
+    def assert_client_state(user, password):
+        mock_client.assert_credentials(user, password)
+
+        assert mock_client._session is None
+        assert mock_client._keystone_client is None
+
+    mock_client = fuelclient_fixture(mocker)
+    mock_client.initial_credentials("userA", "passwordA")
+    context = mock.Mock(user="userB", password="passwordB",
+                        spec=["user", "password"])
+
+    with auth_context(context):
+        assert_client_state(context.user, context.password)
+
+        mock_client._session = mock.Mock()
+        mock_client._keystone_client = mock.Mock()
+
+    assert_client_state("userA", "passwordA")
