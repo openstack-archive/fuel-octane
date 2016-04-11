7f3beac801145277ac99c51b76bff0f44eaf42e0
Sergey Abramov <sabramov@mirantis.com>
2016-04-11 17:08:53 +0300

    Add not live migration case in upgrade node
    with argument --no-live-migration you may start command
    upgrade-node and upgrade more than 1 compute and more than 1 ceph-osd in
    one command
    
    USAGE:
    
    octane upgrade-node --no-live-migration $SEED_ID <ID> <ID> <ID>
    
    Change-Id: I77fc7404131ebc8d30cf53a39c63e48c00481188

--- octane/handlers/upgrade/ceph_osd.py
+++ octane/handlers/upgrade/ceph_osd.py
@@ -17,17 +17,32 @@ from octane.util import puppet
 
 
 class CephOsdUpgrade(upgrade.UpgradeHandler):
+    __RUN_PREPARE = set()
+    __RUN_POSTDEPLOY = set()
+
     def preupgrade(self):
         ceph.check_cluster(self.node)
 
     def prepare(self):
         self.preserve_partition()
-        ceph.set_osd_noout(self.env)
-        puppet.patch_modules()
+        # patch only on first prepare run
+        is_first_run = not self.__class__.__RUN_PREPARE
+        if self.env.data['id'] not in self.__class__.__RUN_PREPARE:
+            self.__class__.__RUN_PREPARE.add(self.env.data['id'])
+            ceph.set_osd_noout(self.env)
+        if is_first_run:
+            puppet.patch_modules()
+            self.__class__.__RUN_POSTDEPLOY = set()
 
     def postdeploy(self):
-        ceph.unset_osd_noout(self.env)
-        puppet.patch_modules(revert=True)
+        # revert only on first postdeploy run
+        is_first_run = not self.__class__.__RUN_POSTDEPLOY
+        if self.env.data['id'] not in self.__class__.__RUN_POSTDEPLOY:
+            self.__class__.__RUN_POSTDEPLOY.add(self.env.data['id'])
+            ceph.unset_osd_noout(self.env)
+        if is_first_run:
+            puppet.patch_modules(revert=True)
+            self.__class__.__RUN_PREPARE = set()
 
     def preserve_partition(self):
         partition = 'ceph'
