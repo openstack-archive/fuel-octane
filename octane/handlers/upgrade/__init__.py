3ea36976ba91d7afd7eec018bbc193897defb1ef
Sergey Abramov <sabramov@mirantis.com>
2016-04-11 17:08:53 +0300

    Add not live migration case in upgrade node
    with argument --no-live-migration you may start command
    upgrade-node and upgrade more than 1 compute and more than 1 ceph-osd in
    one command
    
    USAGE:
    
    octane upgrade-node --no-live-migration $SEED_ID <ID> <ID> <ID>
    
    Change-Id: I77fc7404131ebc8d30cf53a39c63e48c00481188

--- octane/handlers/upgrade/__init__.py
+++ octane/handlers/upgrade/__init__.py
@@ -14,12 +14,13 @@ from octane import handlers
 
 
 class UpgradeHandler(object):
-    def __init__(self, node, env, isolated):
+    def __init__(self, node, env, isolated, live_migration=True):
         self.node = node
         self.orig_env = self.node.env
         self.orig_version = self.orig_env.data["fuel_version"]
         self.env = env
         self.isolated = isolated
+        self.live_migration = live_migration
 
     def preupgrade(self):
         raise NotImplementedError('preupgrade')
