d59198e410b09fcc76b4f97a4add43adb0ec06a1
Ilya Kharin <akscram@gmail.com>
2016-04-20 11:08:08 +0300

    Add re-initialization of the master node
    PuppetApplyHost is renamed to PuppetApplyTasks to perform full
    re-initialization of the master node by applying all puppet tasks.
    
    Change-Id: I52a02fd4e72c1fd6367f91f088beacc0e9715969

--- octane/tests/test_archivators_restore.py
+++ octane/tests/test_archivators_restore.py
@@ -527,14 +527,14 @@ def test_repair_database_consistency(mocker, mock_open):
     ])
 
 
-def test_post_restore_puppet_apply_host(mocker):
+def test_post_restore_puppet_apply_tasks(mocker):
     context = backup_restore.NailgunCredentialsContext(
         user="admin", password="user_pswd")
     mock_set_astute_password = mocker.patch(
         "octane.util.auth.set_astute_password")
-    mock_apply = mocker.patch("octane.util.puppet.apply_host")
+    mock_apply = mocker.patch("octane.util.puppet.apply_all_tasks")
 
-    archivator = puppet.PuppetApplyHost(None, context)
+    archivator = puppet.PuppetApplyTasks(None, context)
     archivator.restore()
 
     assert mock_apply.called
