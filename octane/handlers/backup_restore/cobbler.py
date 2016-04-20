de29427f406cb6b66b48a60bf3d0ff90eca66b33
Sergey Abramov <sabramov@mirantis.com>
2016-04-20 13:57:11 +0300

    Add cobbler profile and distros backup
    Change-Id: Id9323cbd5f5b73451e5d97ffd4191155c5f5dd5f

--- octane/handlers/backup_restore/cobbler.py
+++ octane/handlers/backup_restore/cobbler.py
@@ -14,12 +14,33 @@ from octane.handlers.backup_restore import base
 from octane.util import docker
 
 
-class CobblerArchivator(base.ContainerArchivator):
+class CobblerSystemArchivator(base.ContainerArchivator):
     backup_directory = "/var/lib/cobbler/config/systems.d/"
     banned_files = ["default.json"]
     container = "cobbler"
 
-    def restore(self):
-        super(CobblerArchivator, self).restore()
+
+class CobblerProfileArchivator(base.ContainerArchivator):
+    backup_directory = "/var/lib/cobbler/config/profiles.d/"
+    banned_files = ["bootstrap.json", "ubuntu_bootstrap.json"]
+    container = "cobbler"
+    backup_name = "cobbler_profiles"
+
+
+class CobblerDistroArchivator(base.ContainerArchivator):
+    backup_directory = "/var/lib/cobbler/config/distros.d/"
+    banned_files = ["bootstrap.json", "ubuntu_bootstrap.json"]
+    container = "cobbler"
+    backup_name = "cobbler_distros"
+
+
+class CobblerArchivator(base.CollectionArchivator):
+
+    archivator_classes = [
+        CobblerSystemArchivator,
+        CobblerProfileArchivator,
+        CobblerDistroArchivator]
+
+    def extra_restore(self):
         docker.stop_container("cobbler")
         docker.start_container("cobbler")
