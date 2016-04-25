f5ec212f9862c2e4a07160b0b3861b3e11cd6a30
Sergey Abramov <sabramov@mirantis.com>
2016-04-25 18:03:37 +0300

    Move sql container runner into util function
    This function is used to execute SQL-queries by using psql
    in the postgres container.
    
    Usage:
    
        from octane.util import sql
    
        sql_query = ...
        sql.run_psql_in_container(sql_query, container, db)
    
    Change-Id: I180a0179f223fa59bd0a99cd90b093a153b6ddfe

---
+++ octane/tests/test_util_sql.py
@@ -0,0 +1,45 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+import pytest
+
+from octane.util import sql
+from octane.util import subprocess
+
+
+@pytest.mark.parametrize("sql_raw, result_data", [
+    ("row_1|val_1\nrow_2|val_1\n", ["row_1|val_1", "row_2|val_1"]),
+    ("", [])
+])
+@pytest.mark.parametrize("container", ["postgres"])
+@pytest.mark.parametrize("db", ["nailgun", "keystone"])
+def test_run_sql(mocker, sql_raw, result_data, container, db):
+    run_mock = mocker.patch(
+        "octane.util.docker.run_in_container",
+        return_value=(sql_raw, None))
+    test_sql = "test_sql"
+    results = sql.run_psql_in_container(test_sql, db)
+    run_mock.assert_called_once_with(
+        container,
+        [
+            "sudo",
+            "-u",
+            "postgres",
+            "psql",
+            db,
+            "--tuples-only",
+            "--no-align",
+            "-c",
+            test_sql,
+        ],
+        stdout=subprocess.PIPE
+    )
+    assert result_data == results
