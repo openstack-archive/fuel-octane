94a8cb4adcf1776fd31cacc0704d8ceb5ee570aa
Mykola Yakovliev <vegasq@gmail.com>
2016-04-26 15:14:18 +0300

    Delete old images during restoring
    Remove exists images during restore call because
    they are contains old ssh keys and can't be used
    anymore.
    
    Change-Id: Ib8887621d2fef69c2ee00ebad486415a9be476d3
    Closes-Bug: #1558057

--- octane/util/fuel_bootstrap.py
+++ octane/util/fuel_bootstrap.py
@@ -10,21 +10,16 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from octane.handlers.backup_restore import base
-from octane.util import fuel_bootstrap
+import json
+
 from octane.util import subprocess
 
 
-class SshArchivator(base.PathArchivator):
-    path = "/root/.ssh/"
-    name = "ssh"
+def get_images_uuids():
+    fuel_bootstrap_list = ["fuel-bootstrap", "list", "--format", "json"]
+    images = json.loads(subprocess.call_output(fuel_bootstrap_list))
+    return [img["uuid"] for img in images if img['status'] == "active"]
 
-    def restore(self):
-        super(SshArchivator, self).restore()
-        subprocess.call(
-            ["fuel-bootstrap", "build", "--activate"],
-            env=self.context.get_credentials_env())
 
-        # Remove old images cause they were created with old ssh keys pair
-        for image_uuid in fuel_bootstrap.get_images_uuids():
-            fuel_bootstrap.delete_image(image_uuid)
+def delete_image(uuid):
+    subprocess.call(["fuel-bootstrap", "delete", uuid])
