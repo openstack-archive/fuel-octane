060b468a528dda411793759f4f09e2b7d4acdb98
Sergey Abramov <sabramov@mirantis.com>
2016-05-04 16:53:27 +0300

    Change environment settings on upgrade-env cmd
    set up provision image_data uri based on release version (before that
    them copied from original env)
    
    Change-Id: Ibb7400d2b98ca81fd2a58ca76b04a3e80d4e76a5

--- octane/util/env.py
+++ octane/util/env.py
@@ -26,6 +26,7 @@ from fuelclient.objects import task as task_obj
 from octane.helpers import tasks as tasks_helpers
 from octane.helpers import transformations
 from octane import magic_consts
+from octane.util import sql
 from octane.util import ssh
 from octane.util import subprocess
 
@@ -81,6 +82,33 @@ def change_env_settings(env_id, master_ip=''):
     if get_env_provision_method(env) != 'image':
         attrs['editable']['provision']['method']['value'] = 'image'
     env.update_attributes(attrs)
+    generated_data = sql.run_sql_on_container(
+        "select generated from attributes where cluster_id={0}".format(env_id),
+        "postgres",
+        "nailgun"
+    )[0]
+    generated_data = json.loads(generated_data)
+    release_data = sql.run_sql_on_container(
+        "select attributes_metadata from  releases where id={0}".format(
+            env.release_id),
+        "postgres",
+        "nailgun"
+    )[0]
+    release_image_data_dict = json.loads(
+        release_data
+    )['generated']['provision']['image_data']
+
+    for key, value in generated_data['provision']['image_data'].viewvalues():
+        value['uri'] = release_image_data_dict[key].format({
+            "settings.MASTER_IP": master_ip,
+            "cluster.id": env_id,
+        })
+    sql.run_sql_on_container(
+        "update attributes set generated={0} where cluster_id={1}".format(
+            json.dumps(generated_data), env_id),
+        "postgres",
+        "nailgun"
+    )
 
 
 def clone_env(env_id, release):
