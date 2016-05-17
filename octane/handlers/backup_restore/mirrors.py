3cf623570c34abaada73c45cf5e863696c47342a
Sergey Abramov <sabramov@mirantis.com>
2016-05-17 12:32:08 +0300

    Change sql call n baclup mirrors
    Change sql call from docker run in postgres conatiner to util
    run_psql_in_container function
    
    Change-Id: If1fa514f9b7d67c5d78cdfcc54bdba919ecc8834

--- octane/handlers/backup_restore/mirrors.py
+++ octane/handlers/backup_restore/mirrors.py
@@ -16,9 +16,8 @@ import urlparse
 
 from octane.handlers.backup_restore import base
 
-from octane.util import docker
 from octane.util import helpers
-from octane.util import subprocess
+from octane.util import sql
 
 
 class NaigunWWWBackup(base.PathArchivator):
@@ -32,23 +31,7 @@ class NaigunWWWBackup(base.PathArchivator):
 
     def backup(self):
         ipaddr = helpers.get_astute_dict()["ADMIN_NETWORK"]["ipaddress"]
-        results, _ = docker.run_in_container(
-            "postgres",
-            [
-                "sudo",
-                "-u",
-                "postgres",
-                "psql",
-                self.db,
-                "--tuples-only",
-                "-c",
-                self.sql
-            ],
-            stdout=subprocess.PIPE)
-        results = results.strip()
-        if not results:
-            return
-        rows = results.split("\n")
+        rows = sql.run_psql_in_container(self.sql, self.db)
         already_backuped = set()
         for line in rows:
             data = json.loads(line)
