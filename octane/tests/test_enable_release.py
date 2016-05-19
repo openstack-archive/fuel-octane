fb52bfd2435d1400303c89b8eb4cbc3413402231
Oleg Gelbukh <ogelbukh@mirantis.com>
2016-05-19 16:54:02 +0300

    Add enable-release command
    Change-Id: I625db20e570b177cb46143818ac65c5d78cda0e2

---
+++ octane/tests/test_enable_release.py
@@ -0,0 +1,40 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import pytest
+
+
+@pytest.mark.parametrize("release_id,password",
+                         [('1', 'test_pass'),
+                          ('1', ''),
+                          ('', '')])
+def test_parser(mocker, octane_app, release_id, password):
+    command = "enable-release"
+    get_context_mock = mocker.patch(
+        "octane.commands.enable_release.EnableReleaseCommand.get_context")
+    context_mock = mocker.patch(
+        "octane.handlers.backup_restore.NailgunCredentialsContext")
+    get_context_mock.return_value = context_mock
+    enable_release_mock = mocker.patch(
+        "octane.commands.enable_release.enable_release")
+    params = [command, "--id", release_id, "--admin-password", password]
+    if release_id and password:
+        try:
+            octane_app.run(params)
+        except Exception:
+            raise
+        else:
+            enable_release_mock.assert_called_once_with(release_id,
+                                                        context_mock)
+    else:
+        with pytest.raises(AssertionError):
+            octane_app.run(params)
