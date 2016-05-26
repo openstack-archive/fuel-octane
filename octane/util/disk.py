26743e75a95d76f4a2a7cc42ab3f42e790efebfc
Oleg Gelbukh <ogelbukh@mirantis.com>
2016-05-26 13:07:52 +0000

    Improve partitions and disk metadata handling
    Partitions schema and disk metadata at nodes must be updated only
    if they are scheduled for reprovisioning, and the provisioning
    method has changed to incompatible (i.e. from 'cobbler' to 'image')
    due to the shift of release version.
    
    Move logic that manages partitions from upgrade handlers to function
    that actually initiates the re-provisioning of node. This ensures
    that for every node this is applied only once and only if necessary.
    
    Change-Id: I28718564543e3ccd92d985bd79c76e509e8eae1f
    Closes-bug: 1585938
    Closes-bug: 1575054

--- octane/util/disk.py
+++ octane/util/disk.py
@@ -11,6 +11,7 @@
 # under the License.
 import os.path
 
+from octane import magic_consts
 from octane.util import docker
 from octane.util import ssh
 
@@ -51,5 +52,6 @@ def create_configdrive_partition(node):
         raise Exception("No disks info was found "
                         "for node {0}".format(node.data["id"]))
     # it was agreed that 10MB is enough for config drive partition
-    size = 10
-    create_partition(disks[0]['name'], size, node)
+    create_partition(disks[0]['name'],
+                     magic_consts.CONFIGDRIVE_PART_SIZE,
+                     node)
