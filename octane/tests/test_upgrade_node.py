e725cc2d8c2b8a83ce4d5fce7734b21bccb4ae8d
Oleg Gelbukh <ogelbukh@mirantis.com>
2016-05-26 14:54:41 +0000

    Validate nodes roles to exclude upgrade of single controller
    The proper sequence to upgrade controllers is:
    
    - upgrade primary controller in isolation
    - cut over to the new control plane
    - upgrade remaining controllers at once
    
    To exclude the possibility to upgrade single controller after
    the cutover, add validation of roles of nodes to upgrade and
    stop if the only node passed to the script is a CIC.
    
    Change-Id: Ie91ae35acb7e36103004a59c92cb495837e0dca0
    Closes-bug: 1584656

--- octane/tests/test_upgrade_node.py
+++ octane/tests/test_upgrade_node.py
@@ -10,7 +10,10 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import mock
 import pytest
+
+from octane.commands import upgrade_node
 
 
 @pytest.mark.parametrize('cmd,env,nodes,provision,roles', [
@@ -29,3 +32,55 @@ def test_parser(mocker, octane_app, cmd, env, nodes, provision, roles):
     assert not octane_app.stderr.getvalue()
     m.assert_called_once_with(env, nodes, isolated=True, network_template=None,
                               provision=provision, roles=roles)
+
+
+@pytest.mark.parametrize(
+    "node_ids,isolated,network_template,provision,roles",
+    [(['test-node-1', 'test-node-2', 'test-node-3'],
+      False, None, True, None), ])
+def test_upgrade_node(mocker, node_ids, isolated, network_template,
+                      provision, roles):
+
+    def _create_node(node_id):
+        node = mock.Mock('node', spec_set=['data', 'id'])
+        node.id = node_id
+        node.data = {}
+        node.data['id'] = node_id
+        node.data['cluster'] = None
+        node.data['roles'] = 'controller'
+        return node
+
+    test_env_id = 'test-env'
+    mock_env = mocker.patch("fuelclient.objects.environment.Environment")
+    mock_env.id = test_env_id
+    mock_env.data = {}
+    mock_env.data['id'] = mock_env.id
+    mocker.patch("octane.util.patch.applied_patch")
+    mock_node = mocker.patch("fuelclient.objects.node.Node")
+    mock_node.side_effect = _create_node
+    mock_copy_patches = mocker.patch(
+        "octane.commands.upgrade_node.copy_patches_folder_to_nailgun")
+    mock_get_handlers = mocker.patch(
+        "octane.handlers.upgrade.get_nodes_handlers")
+    mock_handlers = mock_get_handlers.return_value
+    mock_move_nodes = mocker.patch("octane.util.env.move_nodes")
+    mock_copy_vips = mocker.patch("octane.util.env.copy_vips")
+    mock_set_network_template = mocker.patch(
+        "octane.util.env.set_network_template")
+    mock_deploy_nodes = mocker.patch("octane.util.env.deploy_nodes")
+    mock_deploy_changes = mocker.patch("octane.util.env.deploy_changes")
+    upgrade_node.upgrade_node(test_env_id, node_ids)
+
+    mock_copy_patches.assert_called_once()
+    mock_copy_vips.assert_called_once_with(mock_env.return_value)
+    mock_move_nodes.assert_called_once()
+    assert mock_handlers.call_args_list == [
+        mock.call('preupgrade'), mock.call('prepare'),
+        mock.call('predeploy'), mock.call('postdeploy')]
+    if network_template:
+        mock_set_network_template.assert_called_once_with(
+            mock_env.return_value, network_template)
+    if isolated:
+        mock_deploy_nodes.assert_called_once()
+    else:
+        mock_deploy_changes.assert_called_once()
