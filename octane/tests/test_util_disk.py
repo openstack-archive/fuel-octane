d9b19446b258c383763227b30a6b2255ba18a21a
Oleg Gelbukh <ogelbukh@mirantis.com>
2016-05-27 14:23:55 +0000

    Replace ssh.call with ssh.call_output for listing partitions
    Going from ssh.call to ssh.call_output simplifies code and testing.
    
    Change-Id: I5d115ccd1ae69f6b11434ca8f3ba84ef3b3aa049
    Related-bug: 1575054

---
+++ octane/tests/test_util_disk.py
@@ -0,0 +1,47 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import os
+import pytest
+
+from octane.util import disk as disk_util
+
+
+@pytest.mark.parametrize("size,disk", [
+    (1, 'disk'),
+    (0, 'disk'),
+])
+def test_create_partition(mocker, mock_ssh_call, mock_ssh_call_output, node,
+                          size, disk):
+    mock_part_end = mocker.patch("octane.util.disk.parse_last_partition_end")
+    mock_part_end.return_value = 1
+
+    disk_util.create_partition(disk, size, node)
+    mock_ssh_call_output.assert_called_once_with(
+        ['parted', '/dev/%s' % disk, 'unit', 'MB', 'print'], node=node)
+    mock_ssh_call.assert_called_once_with(
+        ['parted', '/dev/%s' % disk, 'unit', 'MB', 'mkpart',
+         'custom', 'ext4', str(2), str(2 + size)], node=node)
+
+
+def test_update_partition_info(mocker, node):
+    test_node_id = 1
+    container = 'nailgun'
+    mock_run_in_container = mocker.patch(
+        "octane.util.docker.run_in_container")
+    expected_command = [
+        'python',
+        os.path.join('/tmp', 'update_node_partition_info.py'),
+        str(test_node_id),
+    ]
+    disk_util.update_node_partition_info(test_node_id)
+    mock_run_in_container.assert_called_once_with(container, expected_command)
