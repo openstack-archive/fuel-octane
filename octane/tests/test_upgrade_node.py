b3dcb0e448bdfa95f4c46ca3180ebb1c4253d802
Oleg Gelbukh <ogelbukh@mirantis.com>
2016-06-06 12:58:15 +0000

    Add function for checking sanity of nodes to upgrade
    Move sanity checks logic for upgrading node into separate
    function.
    
    Change-Id: Ifa46753f0bed18a0e25887c2a07445145011dad4
    Related-bug: 1583063
    (cherry picked from commit 86a7e912a3efe00dc686014d17060ab001638b8a)

--- octane/tests/test_upgrade_node.py
+++ octane/tests/test_upgrade_node.py
@@ -10,7 +10,10 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import mock
 import pytest
+
+from octane.commands import upgrade_node
 
 
 @pytest.mark.parametrize('live_migration', [True, False])
@@ -25,3 +28,89 @@ def test_parser(mocker, octane_app, live_migration):
     m.assert_called_once_with(
         1, [2, 3],
         isolated=True, network_template=None, live_migration=live_migration)
+
+
+@pytest.mark.parametrize(
+    "node_ids,isolated,network_template,provision,roles",
+    [(['test-node-1', 'test-node-2', 'test-node-3'],
+      False, None, True, None), ])
+def test_upgrade_node(mocker, node_ids, isolated, network_template,
+                      provision, roles):
+
+    def _create_node(node_id):
+        node = mock.Mock('node', spec_set=['data', 'id'])
+        node.id = node_id
+        node.data = {}
+        node.data['id'] = node_id
+        node.data['cluster'] = None
+        node.data['roles'] = 'controller'
+        return node
+
+    test_env_id = 'test-env'
+    mock_env = mocker.patch("fuelclient.objects.environment.Environment")
+    mock_env.id = test_env_id
+    mock_env.data = {}
+    mock_env.data['id'] = mock_env.id
+    mocker.patch("octane.util.patch.applied_patch")
+    mock_node = mocker.patch("fuelclient.objects.node.Node")
+    mock_node.side_effect = _create_node
+    mock_copy_patches = mocker.patch(
+        "octane.commands.upgrade_node.copy_patches_folder_to_nailgun")
+    mock_get_handlers = mocker.patch(
+        "octane.handlers.upgrade.get_nodes_handlers")
+    mock_handlers = mock_get_handlers.return_value
+    mock_move_nodes = mocker.patch("octane.util.env.move_nodes")
+    mock_set_network_template = mocker.patch(
+        "octane.util.env.set_network_template")
+    mock_deploy_nodes = mocker.patch("octane.util.env.deploy_nodes")
+    mock_deploy_changes = mocker.patch("octane.util.env.deploy_changes")
+    upgrade_node.upgrade_node(test_env_id, node_ids)
+
+    mock_copy_patches.assert_called_once()
+    mock_move_nodes.assert_called_once()
+    assert mock_handlers.call_args_list == [
+        mock.call('preupgrade'), mock.call('prepare'),
+        mock.call('predeploy'), mock.call('postdeploy')]
+    if network_template:
+        mock_set_network_template.assert_called_once_with(
+            mock_env.return_value, network_template)
+    if isolated:
+        mock_deploy_nodes.assert_called_once()
+    else:
+        mock_deploy_changes.assert_called_once()
+
+
+@pytest.mark.parametrize('node_data,expected_error', [
+    ([{
+        'id': 'test-node',
+        'cluster': None,
+    }], None),
+    ([{
+        'id': 'test-node',
+        'cluster': 'test-env',
+    }], Exception),
+    ([{
+        'id': 'test-node',
+        'cluster': 'test-env-1',
+    }, {
+        'id': 'another-test-node',
+        'cluster': 'test-env-2'
+    }], Exception),
+])
+def test_check_sanity(mocker, node, node_data, expected_error):
+    test_env_id = "test-env"
+    mock_nodes = []
+    for data in node_data:
+        mock_node = mocker.Mock(data=data)
+        mock_nodes.append(mock_node)
+    if expected_error:
+        with pytest.raises(expected_error) as exc_info:
+            upgrade_node.check_sanity(test_env_id, mock_nodes)
+        if len(mock_nodes) == 1:
+            assert "Cannot upgrade node with ID %s:" \
+                in exc_info.value.args[0]
+        else:
+            assert "Not upgrading nodes from different clusters" \
+                in exc_info.value.args[0]
+    else:
+        assert upgrade_node.check_sanity(test_env_id, mock_nodes) is None
