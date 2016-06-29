013ab75540238c656f5d1daec1336fbc6a540c05
Pavel Chechetin <pchechetin@mirantis.com>
2016-06-29 13:16:31 +0300

    Add MongoHandler and handle ceilometer-api service
      * Add a patch for mongo role. In Fuel 7.0 mongo manifests have been
        modified to stay as close as possible to upstream. As side effect
        of this changes couple of options have been set to different
        values. This patch sets those options to the previous values.
    
      * Add `restart ceilometer-api` in upgrade-control command.
        Ceilometer should be restarted after connectivity between
        the MongoDB node and the controller is created again
        (assuming that the controller had been upgraded w/ --isolated).
        Without this step, ceilometer-api serves 500 error code on
        some requests (e.g. ceilometer meter-list).
        Presumably, ceilometer-api behaves so due to starting up w/o
        connectivity with the MongoDB node.
    
    Change-Id: I49c47793b5d8a7cfe791c3e16e5cd86ad02ccbab

---
+++ octane/handlers/upgrade/mongo.py
@@ -0,0 +1,60 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from distutils import version
+import logging
+
+from octane.handlers import upgrade
+from octane import magic_consts
+from octane.util import disk
+from octane.util import mongo as mongo_util
+from octane.util import node as node_util
+from octane.util import puppet
+
+LOG = logging.getLogger(__name__)
+
+
+class MongoUpgrade(upgrade.UpgradeHandler):
+    def prepare(self):
+        mongo_util.check_cluster(self.node)
+
+        mongo_legacy_version = version.StrictVersion(
+            magic_consts.MONGO_LEGACY_CONF_VERSION)
+        fuel_version = version.StrictVersion(
+            self.node.env.data['fuel_version'])
+        if fuel_version <= mongo_legacy_version:
+            puppet.patch_modules()
+
+        self.preserve_partition()
+
+    def postdeploy(self):
+        mongo_legacy_version = version.StrictVersion(
+            magic_consts.MONGO_LEGACY_CONF_VERSION)
+        fuel_version = version.StrictVersion(
+            self.node.env.data['fuel_version'])
+        if fuel_version <= mongo_legacy_version:
+            puppet.patch_modules(revert=True)
+
+    # TODO(ogelbukh): move this action to base handler and set a list of
+    # partitions to preserve as an attribute of a role.
+    def preserve_partition(self):
+        partition = 'mongo'
+        node_util.preserve_partition(self.node, partition)
+
+    def create_configdrive_partition(self):
+        disks = disk.get_node_disks(self.node)
+        if not disks:
+            raise Exception("No disks info was found "
+                            "for node {0}".format(self.node["id"]))
+        # it was agreed that 10MB is enough for config drive partition
+        size = 10
+        disk.create_partition(disks[0]['name'], size, self.node)
