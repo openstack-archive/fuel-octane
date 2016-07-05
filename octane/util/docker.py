d800b1ea18c1b67444e138c8f1111a1fd90b8c02
Sergey Abramov <sabramov@mirantis.com>
2016-07-05 16:45:50 +0300

    Add destroied container context manager
    Required for running destroy docker contaier before code block and
    startup it after block
    
    Usage:
    
        from octane.util import docker
    
        with docker.destoied_container("container_name"):
           ...
    
    Change-Id: Iab57665d2b345bc19f04fad4c0cd4a4e461edd89

--- octane/util/docker.py
+++ octane/util/docker.py
@@ -187,6 +187,17 @@ def start_container(container):
     _container_action(container, "start")
 
 
+@contextlib.contextmanager
+def destroied_container(container):
+    name = get_docker_container_name(container)
+    subprocess.call(["dockerctl", "destroy", name])
+    try:
+        yield
+    finally:
+        subprocess.call(["dockerctl", "start", container])
+        subprocess.call(["dockerctl", "check", container])
+
+
 def wait_for_container(container, attempts=120, delay=5):
     assert delay > 0
     _wait_for_start_container(container, attempts, delay)
