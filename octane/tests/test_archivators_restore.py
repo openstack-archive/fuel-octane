1e6bc80ee323cec2be1febd646b9e8ca170afa1d
Sergey Abramov <sabramov@mirantis.com>
2016-07-05 12:08:45 +0300

    Stop all docker containers using docker destroy all
    Required for stop and start docker containers
    in order listed by dockerctl.
    
    Change-Id: Ifb8cf38f3151bff19891d7877180b99406acb06b
    Closes-bug: 1599094

--- octane/tests/test_archivators_restore.py
+++ octane/tests/test_archivators_restore.py
@@ -472,21 +472,12 @@ def test_astute_restore(mocker, mock_open, keys_in_dump_file, restored):
 
 
 def test_post_restore_action_astute(mocker):
-
-    stopped = []
-    mocker.patch(
-        "octane.util.docker.get_docker_container_names",
-        return_value=["container_1", "container_2"]
-    )
-    start = mocker.patch("octane.util.docker.start_container",
-                         side_effect=stopped.remove)
-    stop = mocker.patch("octane.util.docker.stop_container",
-                        side_effect=stopped.append)
+    subporcess_mock = mocker.patch("octane.util.subprocess.call")
 
     astute.AstuteArchivator(None)._post_restore_action()
-    assert start.called
-    assert stop.called
-    assert not stopped
+    actions = ["destroy", "start", "check"]
+    calls = [mock.call(["dockerctl", a, "all"]) for a in actions]
+    assert calls == subporcess_mock.call_args_list
 
 
 @pytest.mark.parametrize("dump, calls, data_for_update", [
