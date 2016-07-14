f4808cc66d718edd12ac5a3643429daf723f7674
Nikita Zubkov <nzubkov@mirantis.com>
2016-07-14 16:54:54 +0300

    For non-root user run sftp server as root
    To be able to get access to all files on fs.
    
    Change-Id: I3a106ff75023acfc9ce79c90efde5d07ba0dce3f
    Partial-Bug: #1575477

--- octane/util/ssh.py
+++ octane/util/ssh.py
@@ -182,6 +182,13 @@ def call_output(cmd, **kwargs):
 @_cache
 def _get_sftp(node):
     transport = get_client(node).get_transport()
+    username = transport.auth_handler.username
+    if username != 'root':
+        LOG.info('Run sftp server as root')
+        channel = transport.open_channel('session')
+        channel.exec_command('sudo ' + magic_consts.SFTP_SERVER_BIN)
+        return paramiko.SFTPClient(channel)
+
     return paramiko.SFTPClient.from_transport(transport)
 
 get_client.invalidate.append(_get_sftp)
