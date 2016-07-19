a41f16632824e01073a163233520c55248491dcd
Sergey Abramov <sabramov@mirantis.com>
2016-07-19 17:10:44 +0300

    Change shutoff vm command
    * run nova commands using NovaRunner
    * host filter using get nova node handle method(
        hostname for fuel < 6.1 and fqdn for newer version)
    
    Change-Id: Ib5c373714e359982b13910a225787167db7b35e9

--- octane/handlers/upgrade/compute.py
+++ octane/handlers/upgrade/compute.py
@@ -103,15 +103,15 @@ class ComputeUpgrade(upgrade.UpgradeHandler):
 
     def shutoff_vms(self):
         password = env_util.get_admin_password(self.env)
-        controller = env_util.get_one_controller(self.env)
-        cmd = ['. /root/openrc;',
-               'nova --os-password {0} list --host {1}'
-               .format(password, self.node.data['hostname']),
-               '|',
-               "awk -F\| '$4~/ACTIVE/{print($2)}'",
-               '|',
-               'xargs -I% nova stop %']
-        ssh.call(["sh", "-c", ' '.join(cmd)], stdout=ssh.PIPE, node=controller)
+        runner = nova.NovaRunner(self.env)
+        node_fqdn = node_util.get_nova_node_handle(self.node)
+        instances_str = runner.call_output([
+            "nova", "--os-password", password, "list", "--host", node_fqdn,
+            "|", "awk -F\| '$4~/ACTIVE/{print($2)}'"])
+        instances = instances_str.strip().splitlines()
+        for instance in instances:
+            instance = instance.strip()
+            runner.call(["nova", "stop", instance])
 
     def backup_iscsi_initiator_info(self):
         if not plugin.is_enabled(self.env, 'emc_vnx'):
