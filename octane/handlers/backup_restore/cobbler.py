b9a4b99e139305dcc0a697c7fa5ccb7b6e3ffc6f
Ilya Kharin <akscram@gmail.com>
2016-08-04 02:02:44 +0300

    Use ubuntu_bootstrap for old systems
    The bootstrap profile is no more used on the 9.0 Fuel Master node and
    for Cobbler systems that were created in 7.0 the profile attribute
    should be renamed to the default bootstrap profile
    
    Change-Id: I562506f2d457ba60df66afb0bc1ecc4bc30fd38d
    Closes-Bug: #1602583

--- octane/handlers/backup_restore/cobbler.py
+++ octane/handlers/backup_restore/cobbler.py
@@ -10,7 +10,12 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import json
+import os
+
 from octane.handlers.backup_restore import base
+from octane.util import archivate
+from octane.util import helpers
 from octane.util import puppet
 from octane.util import subprocess
 
@@ -19,6 +24,24 @@ class CobblerSystemArchivator(base.PathFilterArchivator):
     backup_directory = "/var/lib/cobbler/config/systems.d/"
     banned_files = ["default.json"]
     backup_name = "cobbler"
+
+    @staticmethod
+    def get_default_profile():
+        astute = helpers.load_yaml("/etc/astute/astuted.conf")
+        profile = astute.get("bootstrap_profile", "ubuntu_bootstrap")
+        return profile
+
+    def restore(self):
+        default_profile = self.get_default_profile()
+        for member in archivate.filter_members(self.archive, self.backup_name):
+            system = json.load(self.archive.extractfile(member))
+            if system["profile"] == "bootstrap":
+                system["profile"] = default_profile
+            filename = os.path.basename(member.name)
+            extract_path = os.path.join(self.backup_directory, filename)
+            with open(extract_path, "wb") as fp:
+                json.dump(system, fp)
+            os.chmod(extract_path, member.mode)
 
 
 class CobblerProfileArchivator(base.PathFilterArchivator):
