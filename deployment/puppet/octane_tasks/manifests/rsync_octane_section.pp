213c9e11c0c7627c4ed98700ac820a2344c2f840
Pavel Chechetin <pchechetin@mirantis.com>
2016-08-08 16:40:56 +0300

    Automated configuration of rsyncd
    Automated configuration (using custom graph feature) is added.
    rsyncd.conf is configurated using Augeas resource in Puppet as
    an elegent solution which doesn't interfere with existing
    configuration by Fuel bootstrap.
    
    RPM spec is also updated to incorporate octane_tasks Puppet module
    into Octane package.
    
    Untapped controller group is deleted from upgrade-db-orig graph.
    
    Change-Id: Ia8ea7214b0b93f414d1de9b07a48e1602c0d2ae3

---
+++ deployment/puppet/octane_tasks/manifests/rsync_octane_section.pp
@@ -0,0 +1,21 @@
+
+# == Class: octane_tasks::rsync_octane_section
+#
+# This class adds a section to rsyncd.conf for Octane with r/w support.
+# It also restarts Rsyncd if it's needed.
+#
+class octane_tasks::rsync_octane_section {
+  augeas { 'rsync_octane_section' :
+    context => '/files/etc/rsyncd.conf/octane',
+    notify  => Service['rsyncd'],
+    changes => [
+      'set path /var/www/nailgun/octane',
+      'set read\\ only false',
+      'set uid 0',
+      'set gid 0',
+      'set use\\ chroot no',
+    ]
+  }
+
+  service { 'rsyncd': }
+}
