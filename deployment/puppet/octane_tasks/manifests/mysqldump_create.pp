e601f88e4322b090675dff10e5052f39af9900d2
Pavel Chechetin <pchechetin@mirantis.com>
2016-08-17 20:04:18 +0300

    Add encryption for DB transportation
    Change-Id: I715701a079f06e11480738427305d8b1389f6566

---
+++ deployment/puppet/octane_tasks/manifests/mysqldump_create.pp
@@ -0,0 +1,14 @@
+
+# TODO: Create extensive descript for class
+class octane_tasks::mysqldump_create {
+  $nova_hash                  = hiera_hash('nova')
+  $password                   = $nova_hash['db_password']
+  $db_list_command            = 'mysql --silent --batch -e "show databases" | egrep -v "(Database|information_schema|performance_schema|mysql)" | tr "\n" " "'
+  $compress_and_enc_command   = 'gzip | openssl enc -e -aes256 -pass env:PASSWORD -out /var/tmp/dbs.original.sql.gz.enc'
+
+  exec { 'backup_and_encrypt':
+    command     => "mysqldump --defaults-file=/root/.my.cnf --host localhost --add-drop-database --lock-all-tables --databases ${db_list_command} | ${compress_and_enc_command}",
+    provider    => shell,
+    environment => "PASSWORD=${password}",
+  }
+}
