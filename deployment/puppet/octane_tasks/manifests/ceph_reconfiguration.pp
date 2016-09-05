64e0e5c9970514fcdc40e54ff1f9dfc809ea92ec
Pavel Chechetin <pchechetin@mirantis.com>
2016-09-05 16:39:35 +0300

    Graph-based upgrade-ceph. Puppet part.
     - Graphs and puppet manifests part
     - Delete lib from .gitignore
    
    Change-Id: I639cbf786971fea8c56b4da6b2661477b3b12c41

---
+++ deployment/puppet/octane_tasks/manifests/ceph_reconfiguration.pp
@@ -0,0 +1,58 @@
+
+# == Class:octane_tasks::ceph_reconfiguration
+#
+# It replaces fsid by former fsid, using taken from the original controller ceph.conf, in:
+#   - ceph.conf
+#   - Ceph Monmap
+#
+class octane_tasks::ceph_reconfiguration {
+  Exec {
+    provider => shell,
+  }
+
+  $orig_fsid    = ceph_get_fsid('/var/tmp/ceph.conf')
+  $tmp_mon_map  = '/var/tmp/ceph_mon_map'
+
+  validate_re($orig_fsid, '\A[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\z')
+
+  exec { 'extract_map':
+    command => "ceph-mon -i ${::hostname} --extract-monmap ${tmp_mon_map}",
+  }
+
+  exec { 'delete_old_mon_db':
+    command => "rm -rf /var/lib/ceph/mon/ceph-${::hostname}/*",
+    require => Exec['extract_map'],
+  }
+
+  exec { 'extract_db':
+    command => 'tar -xzPf /var/tmp/ceph_mon.tar.gz',
+    cwd     => "/var/lib/ceph/mon/ceph-${::hostname}",
+    require => Exec['delete_old_mon_db'],
+  }
+
+  exec { 'extract_ceph_etc':
+    command => 'tar -xzPf /var/tmp/ceph_etc.tar.gz',
+  }
+
+  augeas { 'ceph.conf':
+    lens    => 'Ceph.lns',
+    incl    => '/etc/ceph/ceph.conf',
+    changes => [
+      "set /files/etc/ceph/ceph.conf/global/fsid ${orig_fsid}",
+    ]
+  }
+
+  exec { 'change_fsid':
+    command => "monmaptool --fsid ${orig_fsid} --clobber ${tmp_mon_map}",
+    require => Exec['extract_map'],
+  }
+
+  exec { 'inject_map':
+    command => "ceph-mon -i ${::hostname} --inject-monmap ${tmp_mon_map}",
+    require => [
+      Exec['change_fsid'],
+      Augeas['ceph.conf'],
+    ],
+  }
+}
+
---
+++ deployment/puppet/octane_tasks/modular/ceph_reconfiguration.pp
@@ -0,0 +1,3 @@
+notice('MODULAR: octane_tasks::ceph_reconfiguration.pp')
+
+include ::octane_tasks::ceph_reconfiguration
