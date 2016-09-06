21eaa3af0da7d1baa8d13b44900c07aee2371b6f
Anastasiya <atolochkova@mirantis.com>
2016-09-06 10:23:39 +0300

    Backup/restore for admin networks
    * backup/restore for /etc/hiera/networks.yaml was added
    * configure dhcp after restore was added
    
    Change-Id: I5b1e3861589e1c56acbc37d0be569da5e55b8536
    Closes-Bug: #1616998

---
+++ octane/handlers/backup_restore/admin_networks.py
@@ -0,0 +1,25 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from octane.handlers.backup_restore import base
+from octane.util import puppet
+
+
+class AdminNetworks(base.PathFilterArchivator):
+    backup_directory = "/etc/hiera/networks.yaml"
+    allowed_files = ["networks.yaml"]
+    backup_name = "networks"
+
+    def restore(self):
+        super(AdminNetworks, self).restore()
+        if self.archive.getmembers():
+            puppet.apply_task("dhcp-ranges")
