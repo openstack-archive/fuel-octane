cff2a431d3f4012e935397625dea8b82bbf39929
Pavel Chechetin <pchechetin@mirantis.com>
2016-09-06 13:13:42 +0300

    Graph-based upgrade-ceph. Puppet part.
     - Graphs and puppet manifests part
     - Delete lib from .gitignore
     - Augeas lens for Ceph is copied and pasted, should be merged
       when [0] is merged and published.
    
    [0] Phttps://github.com/hercules-team/augeas/pull/401
    
    Change-Id: I639cbf786971fea8c56b4da6b2661477b3b12c41

---
+++ deployment/puppet/octane_tasks/manifests/ceph_mon_dump_create.pp
@@ -0,0 +1,20 @@
+# == Class: octane_tasks::ceph_mon_dump_create
+#
+# It creates a dump of Ceph Monitor database.
+#
+class octane_tasks::ceph_mon_dump_create {
+
+  Exec {
+    provider => shell,
+  }
+
+  exec { 'ceph_mon_dump_create':
+    command => 'tar -czPf /var/tmp/ceph_mon.tar.gz  *',
+    cwd     => "/var/lib/ceph/mon/ceph-${::hostname}",
+  }
+
+  exec { 'ceph_etc_dump_create':
+    command  => 'tar -czPf /var/tmp/ceph_etc.tar.gz --exclude ceph.conf /etc/ceph',
+  }
+
+}
---
+++ deployment/puppet/octane_tasks/modular/ceph_bootstrap.pp
@@ -0,0 +1,3 @@
+notice('MODULAR: octane_tasks::ceph_bootstrap.pp')
+
+include ::octane_tasks::ceph_bootstrap
