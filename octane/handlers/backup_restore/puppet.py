5a3aafb4f66ab52757b613c48e24971477006cf8
Nikita Zubkov <nzubkov@mirantis.com>
2016-09-07 18:45:10 +0300

    Check that astute.yaml password is valid before backup/restore
    Change-Id: I0dd79ec624159dd1132ca06a86b35d80941c8c39
    Partial-Bug: #1606613

--- octane/handlers/backup_restore/puppet.py
+++ octane/handlers/backup_restore/puppet.py
@@ -33,7 +33,6 @@ class PuppetApplyTasks(base.Base):
 
     def restore(self):
         subprocess.call(["systemctl", "stop"] + self.services)
-        with auth.set_astute_password(self.context), \
-                keystone.admin_token_auth(magic_consts.KEYSTONE_PASTE,
-                                          magic_consts.KEYSTONE_PIPELINES):
+        with keystone.admin_token_auth(magic_consts.KEYSTONE_PASTE,
+                                       magic_consts.KEYSTONE_PIPELINES):
             puppet.apply_all_tasks()
--- octane/tests/test_util_auth.py
+++ octane/tests/test_util_auth.py
@@ -10,51 +10,25 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import mock
+from fuelclient.client import APIClient
 import pytest
+import requests
 
-from octane.handlers import backup_restore
 from octane.util import auth
 
 
-class TestException(Exception):
-    pass
+@pytest.mark.parametrize("status,valid", [
+    (401, False),
+    (500, requests.HTTPError),
+    (200, True),
+])
+def test_is_creds_valid(mocker, status, valid):
+    response = requests.Response()
+    response.status_code = status
+    mocker.patch.object(APIClient, 'get_request_raw', return_value=response)
 
-
-@pytest.mark.parametrize("exc_on_apply", [True, False])
-def test_set_astute_password(mocker, mock_open, exc_on_apply):
-    fd_mock = mock.Mock()
-    close_mock = mocker.patch("os.close")
-    mkstemp_mock = mocker.patch(
-        "tempfile.mkstemp",
-        return_value=(fd_mock, "/etc/fuel/.astute.yaml.bac"))
-    mock_copy = mocker.patch("shutil.copy2")
-    mock_move = mocker.patch("shutil.move")
-    yaml_load = mocker.patch(
-        "yaml.load", return_value={"FUEL_ACCESS": {"password": "dump_pswd"}})
-    yaml_dump = mocker.patch("yaml.safe_dump")
-    context = backup_restore.NailgunCredentialsContext(
-        user="admin", password="user_pswd")
-    if exc_on_apply:
-        with pytest.raises(TestException):
-            with auth.set_astute_password(context):
-                raise TestException("text exception")
+    if not isinstance(valid, bool):
+        with pytest.raises(valid):
+            auth.is_creds_valid('a', 'b')
     else:
-        with auth.set_astute_password(context):
-            pass
-    assert mock_open.call_args_list == [
-        mock.call("/etc/fuel/astute.yaml", "r"),
-        mock.call("/etc/fuel/astute.yaml", "w"),
-    ]
-    yaml_load.assert_called_once_with(mock_open.return_value)
-    yaml_dump.assert_called_once_with(
-        {'FUEL_ACCESS': {'password': 'user_pswd'}},
-        mock_open.return_value,
-        default_flow_style=False)
-    mock_copy.assert_called_once_with("/etc/fuel/astute.yaml",
-                                      "/etc/fuel/.astute.yaml.bac")
-    mock_move.assert_called_once_with("/etc/fuel/.astute.yaml.bac",
-                                      "/etc/fuel/astute.yaml")
-    mkstemp_mock.assert_called_once_with(
-        dir="/etc/fuel", prefix=".astute.yaml.octane")
-    close_mock.assert_called_once_with(fd_mock)
+        assert auth.is_creds_valid('a', 'b') == valid
