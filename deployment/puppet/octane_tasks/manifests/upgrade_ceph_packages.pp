40c8e5c0325b4ec243039c3cecb03e126b102cd1
Dmitry Nikishov <dnikishov@mirantis.com>
2016-10-17 17:20:54 +0300

    Graph for osd-upgrade procedure
    This commit introduces the task graphs that
    handle osd-upgrade command. Python part is
    in a separate commit.
    
    There are 3 graphs:
    1. (orig) prepare environment: update repos
    2. (seed) set noout & upgrade ceph on target nodes
    3. (orig) unset noout and restart
    
    Change-Id: I0ce93831f715ad919acb4420f05b729afb9bb15b

---
+++ deployment/puppet/octane_tasks/manifests/upgrade_ceph_packages.pp
@@ -0,0 +1,26 @@
+# == class: octane_tasks::upgrade_ceph_packages
+#
+# this class upgrades ceph packages on the current node
+#
+class octane_tasks::upgrade_ceph_packages {
+  $ceph_mon_versions = ceph_get_version('mon')
+  $ceph_osd_versions = ceph_get_version('osd')
+
+  $ceph_release = hiera('ceph_upgrade_release')
+  $node_hostnames_string = join(hiera('ceph_upgrade_hostnames'), " ")
+
+  Exec {
+    provider => shell,
+  }
+
+  if ! ceph_equal_versions($ceph_mon_versions, $ceph_osd_versions) {
+
+    exec { 'upgrade-ceph-packages':
+      command => "ceph-deploy install --release ${ceph_release} ${ceph_hostnames_string}",
+    }
+
+  } else {
+    notice('the version of osd on current node matches mon version, nothing to upgrade.')
+  }
+}
+
